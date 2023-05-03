92d9fcea87b3e1d899a6cc7da91e76d6d14ce2c8
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-05-02 19:43:51 -0700

    Time: 55 ms (5.33%), Space: 16.1 MB (7.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # using DP
-        one, two = 1, 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+        # one, two = 1, 1
+        # for i in range(n-1):
+        #     temp = one
+        #     one = one + two
+        #     two = temp
+        # return one
+        # using fibonacci series approach
+        k1 = 0
+        k2 = 1
+        while n > 0:
+            k1, k2 = k2, k1+k2
+            n-= 1
+        return k2
         
             
             
