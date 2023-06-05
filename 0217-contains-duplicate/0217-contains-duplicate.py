5b6205677d1a6d91be7a2fdfb3d2afe30f9980dd
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-06-05 12:24:00 -0700

    Time: 549 ms (65.61%), Space: 32 MB (17.60%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -8,6 +8,17 @@ class Solution:
         # return False
         
         #one-line solution
-        return len(set(nums)) != len(nums)
+        # return len(set(nums)) != len(nums)
+        
+        #using a set DS
+        nums_set = set()
+        for i in nums:
+            if i in nums_set:
+                return True
+            else:
+                nums_set.add(i)
+        return False
+                
+        
         
                
