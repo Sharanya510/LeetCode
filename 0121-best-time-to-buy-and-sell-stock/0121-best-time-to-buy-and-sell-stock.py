370b4c520cd93f550ae91b920832adabf74cc97e
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-06-29 17:30:19 -0700

    Time: 925 ms (87.64%), Space: 27.3 MB (66.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] < min_value :
                 min_value = nums[i]
-            elif nums[i] - min_value > max_profit:
+            if nums[i] - min_value > max_profit:
                 max_profit = max(max_profit, nums[i] - min_value)
             
                 
