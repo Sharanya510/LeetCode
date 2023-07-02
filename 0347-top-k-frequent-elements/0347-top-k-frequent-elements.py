46f66e58fb2de3ac18fec3287c64c7093cade150
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-02 15:36:57 -0700

    Time: 120 ms (47.06%), Space: 21.2 MB (58.77%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,28 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_hash =  {}
+        res = []
+        for i, n in enumerate(nums):
+            if n not in nums_hash:
+                nums_hash[n] = 1
+            else:
+                nums_hash[n] += 1
+        print(nums_hash)
+        
+        heap = []
+        for key, value in nums_hash.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (value, key))
+            elif len(heap) >= k:
+                if value> heap[0][0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, (value, key))
+        
+        res = []
+        while heap:
+            value, key = heapq.heappop(heap)
+            res.append(key)
+            
+        return res
+            
+                
