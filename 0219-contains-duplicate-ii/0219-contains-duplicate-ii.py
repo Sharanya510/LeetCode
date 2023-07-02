868213a64cbef1047899dd72b66b113f5ab72df0
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-02 14:22:34 -0700

    Time: 601 ms (65.67%), Space: 29.7 MB (23.25%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -9,13 +9,20 @@ class Solution:
         #                 return True
         # return False
         
+        # nums_hash = {}
+        # for i , num in enumerate(nums):
+        #     if num not in nums_hash:
+        #         nums_hash[num] = i
+        #     else:
+        #         if abs(i - nums_hash[num]) <= k :
+        #             return True
+        #         nums_hash[num] = i
+        # return False
+        
         nums_hash = {}
-        for i , num in enumerate(nums):
-            if num not in nums_hash:
-                nums_hash[num] = i
-            else:
-                if abs(i - nums_hash[num]) <= k :
-                    return True
-                nums_hash[num] = i
+        for i in range(len(nums)):
+            if nums[i] in nums_hash and i - nums_hash[nums[i]] <= k:
+                return True
+            nums_hash[nums[i]] = i
         return False
         
