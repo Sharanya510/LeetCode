d0190a7f62554441f5a97b9351b612085d56e756
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-02 16:30:54 -0700

    Time: 57 ms (87.35%), Space: 16.8 MB (63.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,31 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        s_map, t_map = {}, {}
-        for c in s:
-            if c not in s_map:
-                s_map[c] = 1
-            else:
-                s_map[c] += 1
-        for c in t:
-            if c not in t_map:
-                t_map[c] = 1
-            else:
-                t_map[c] += 1
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
         
-        return s_map == t_map
+        
+        #TWO HASHMAPS
+#         s_map, t_map = {}, {}
+#         for c in s:
+#             if c not in s_map:
+#                 s_map[c] = 1
+#             else:
+#                 s_map[c] += 1
+#         for c in t:
+#             if c not in t_map:
+#                 t_map[c] = 1
+#             else:
+#                 t_map[c] += 1
+        
+#         return s_map == t_map
         
         # BRUTE--FORCE
         # if len(s) != len(t):
