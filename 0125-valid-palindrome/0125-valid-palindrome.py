00f71b90e040aaa87c6b587fd77e5e7091fd6ba5
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-02 18:42:47 -0700

    Time: 61 ms (68.67%), Space: 17.1 MB (60.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,38 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res = ""
-        for c in s:
-            if c.isalnum():
-                res += c
-            else:
-                continue
-        # print(res)
-        rev_res = ""
-        for i in range(len(res)-1, -1, -1):
-            if res[i].isupper():
-                rev_res += res[i].lower()
-            else:
-                rev_res += res[i]
-        print(rev_res)
+        l, r = 0, len(s) - 1
+        while l < r :
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
         
-        new_res = rev_res[::-1]
-        print(new_res)
-        return rev_res == new_res
+        
+        
+        # BRUTE--FORCE o(n)
+#         res = ""
+#         for c in s:
+#             if c.isalnum():
+#                 res += c
+#             else:
+#                 continue
+#         # print(res)
+#         rev_res = ""
+#         for i in range(len(res)-1, -1, -1):
+#             if res[i].isupper():
+#                 rev_res += res[i].lower()
+#             else:
+#                 rev_res += res[i]
+#         print(rev_res)
+        
+#         new_res = rev_res[::-1]
+#         print(new_res)
+#         return rev_res == new_res
                 
                 
                 
