f805b964b005b94f35820e2f51ccae13b64d7fe9
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-02 19:08:47 -0700

    Time: 727 ms (65.85%), Space: 29.3 MB (82.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,28 +3,17 @@ class Solution:
         l, r = 0, len(height) - 1
         maxarea = 0
         while l < r:
+            area = (r - l) * min(height[l], height[r])
+            maxarea = max(area, maxarea)
             if height[l] < height[r]:
-                
-                area = (r - l) * min(height[l], height[r])
-                maxarea = max(area, maxarea)
                 l += 1
             elif height[l] > height[r]:
-                area = (r - l) * min(height[l], height[r])
-                maxarea = max(area, maxarea)
                 r -= 1
             else:
-                area = (r - l) * min(height[l], height[r])
-                maxarea = max(area, maxarea)
                 l += 1
                 r -= 1
         
         return maxarea
-#  0 1 2 3 4 5 6 7 8       
-# [1,8,6,2,5,4,8,3,7]
-# first --> l = 0, r = 8 --> area = (8-0)* min(1,7) = 8
-# second--> l = 1, r = 8 --> area = (8-1)* min(7,8) = 49
-# third--> l = 1, r = 7 --> area = (7-1)* min(8,3) = 18
-# fourth--> l = 1, r = 6 --> area = (6-1)* min(8,8) = 40
 
 
         # BRUTE-- FORCE
