2c1e9299785a71a58f31e59eb7b6bcf5020a694f
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-16 00:27:38 -0700

    Time: 157 ms (23.04%), Space: 23 MB (73.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -9,9 +9,13 @@ class Solution:
                 n = n >> 1
             return count
         
-        res = [0] * (n + 1)
-        for i in range(1, n+1):
+        res = [0] * (n + 2)
+        # for i in range(1, n+1):
+        #     res[i] = helper(i)
+        # return res
+        for i in range(0, n+1, 2):
             res[i] = helper(i)
-        return res
+            res[i+1] = res[i] + 1
+        return res[0:n+1]
         
         
