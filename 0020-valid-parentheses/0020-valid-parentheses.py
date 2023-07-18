70e8bf798f7e01b4197899b31c00a5bdcfd81cf9
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-17 18:29:15 -0700

    Time: 51 ms (35.37%), Space: 16.4 MB (55.40%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,57 +1,36 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         my_stack = []
-        mapping = { ")": "(", "}": "{", "]": "["}
-        
         for c in s:
-            if c in mapping:
+            if c == '(':
+                my_stack.append(')')
+            elif c == '[':
+                my_stack.append(']')
+            elif c == '{':
+                my_stack.append('}')
+            elif c == ')':
                 if my_stack:
-                    top = my_stack.pop() 
+                    if my_stack[-1] == ')':
+                        my_stack.pop()
+                    else:
+                        return False
                 else:
                     return False
-                if mapping[c] != top:
+            elif c == ']':
+                if my_stack:
+                    if my_stack[-1] == ']':
+                        my_stack.pop()
+                    else:
+                        return False
+                else:
                     return False
-            else:
-                my_stack.append(c)
-        return not my_stack
-        
-        # my_stack = []
-        # for c in s:
-        #     if c == '{' :
-        #         my_stack.append('}')
-        #     elif c == '[':
-        #         my_stack.append(']')
-        #     elif c == '(':
-        #         my_stack.append(')')
-        #     elif c == ')':
-        #         if my_stack:
-        #             if my_stack[-1] != ')':
-        #                 return False
-        #             else:
-        #                 my_stack.pop()
-        #         else:
-        #             return False
-        #     elif c == ']':
-        #         if my_stack:
-        #             if my_stack[-1] != ']':
-        #                 return False
-        #             else:
-        #                 my_stack.pop()
-        #         else:
-        #             return False
-        #     elif c == '}':
-        #         if my_stack:
-        #             if my_stack[-1] != '}':
-        #                 return False
-        #             else:
-        #                 my_stack.pop()
-        #         else:
-        #             return False
-        # return len(my_stack) == 0
-             
-        
-        
-        
-            
-            
+            elif c == '}':
+                if my_stack:
+                    if my_stack[-1] == '}':
+                        my_stack.pop()
+                    else:
+                        return False
+                else:
+                    return False
+        return len(my_stack) == 0
         
