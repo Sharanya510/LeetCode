693aea6b32af7e8fa7bcd135d1885b9515ccbc42
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-17 17:28:02 -0700

    Time: 183 ms (25.19%), Space: 16.3 MB (80.40%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,6 +2,7 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         hash_map = {}
         length = 0
+        longest = 0
         i = 0
         
         for j in range(len(s)):
@@ -9,11 +10,26 @@ class Solution:
                 hash_map[s[j]] += 1
             else:
                 hash_map[s[j]] = 1
-            
+                
             while j - i + 1 - max(hash_map.values()) > k:
-                hash_map[s[i]] -= 1
-                i += 1
-            length = max(length , j - i + 1)
-        return length
+                hash_map[s[i]]-=1
+                i+=1
+            longest=max(longest,j-i+1)
+        return longest
+                
+                
+
+                
+#                 A A B A B B A
+#                 i
+#                         j
+      
+    
+    
+    
+#     {A:3,B:2}
+                    
+                
+                
         
         
