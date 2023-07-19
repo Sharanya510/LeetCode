a80e6ff6c4b62074536f5097b5dcabc198d717b5
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-19 11:36:18 -0700

    Time: 65 ms (13.01%), Space: 16.4 MB (14.01%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -5,38 +5,46 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+#         if not head:
+#             return None
         
-        curr = head
-        while curr.next:
-            if curr.val == curr.next.val:
-                curr.next = curr.next.next
+#         curr = head
+#         while curr.next:
+#             if curr.val == curr.next.val:
+#                 curr.next = curr.next.next
+#             else:
+#                 curr = curr.next
+#         return head
+    
+        temp = head
+        temp_list = []
+        while temp:
+            temp_list.append(temp.val)
+            temp = temp.next
+        print(temp_list)
+        
+        # for i in range(len(temp_list)-2):
+        #     if temp_list[i+1] == temp_list[i]:
+        #         temp_list.remove(temp_list[i])
+        # print(temp_list)
+        i = 0
+        while i < len(temp_list)-1:
+            if temp_list[i] == temp_list[i+1]:
+                del temp_list[i]
             else:
-                curr = curr.next
-        return head
-#         temp = head
-#         temp_list = []
-#         while temp:
-#             temp_list.append(temp.val)
-#             temp = temp.next
-#         print(temp_list)
+                i = i+1
+        print(temp_list)
         
-#         for i in range(len(temp_list)-2):
-#             if temp_list[i+1] == temp_list[i]:
-#                 temp_list.remove(temp_list[i])
-#         print(temp_list)
+        dummy_node = ListNode(-1)
         
-#         dummy_node = ListNode(-1)
+        if temp_list:
+            curr = ListNode(temp_list[0])
+            dummy_node.next = curr
         
-#         if temp_list:
-#             curr = ListNode(temp_list[0])
-#             dummy_node.next = curr
-        
-#         for i in range(len(temp_list)):
-#             curr.next = ListNode(temp_list[i])
-#             curr = curr.next
-#         return dummy_node.next
+        for i in range(1, len(temp_list)):
+            curr.next = ListNode(temp_list[i])
+            curr = curr.next
+        return dummy_node.next
 
         
         
