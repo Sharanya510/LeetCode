0cf0f5c59071e8063c8bf93244c14d1447d3e3fb
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-20 23:14:34 -0700

    Time: 151 ms (37.68%), Space: 18.9 MB (78.51%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        hash_map = defaultdict(int)
+        for n in nums:
+            hash_map[n] += 1
+        for i in hash_map:
+            if hash_map[i] == 1:
+                return i
+        
