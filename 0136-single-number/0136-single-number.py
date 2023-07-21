a614dfa34c8f92b47332458d72694be4a674237e
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-20 23:15:54 -0700

    Time: 143 ms (68.34%), Space: 18.7 MB (98.89%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        hash_map = defaultdict(int)
-        for n in nums:
-            hash_map[n] += 1
-        for i in hash_map:
-            if hash_map[i] == 1:
-                return i
+        # hash_map = defaultdict(int)
+        # for n in nums:
+        #     hash_map[n] += 1
+        # for i in hash_map:
+        #     if hash_map[i] == 1:
+        #         return i
+
+        a = 0
+        for i in nums:
+            a = a^i
+        return a
         
