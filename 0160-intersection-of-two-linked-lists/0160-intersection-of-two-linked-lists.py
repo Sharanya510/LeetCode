3acce475b3129dfb1720b815284da9aa9d838a66
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-21 00:25:52 -0700

    Time: 183 ms (22.19%), Space: 31.6 MB (51.58%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,18 +6,45 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        temp1 = headA
-        temp2 = headB
+        posA = headA
+        posB = headB
+        lenA = self.getLen(headA)
+        lenB = self.getLen(headB)
+
+        if (lenA - lenB > 0):
+            while( lenA != lenB):
+                posA = posA.next
+                lenA -= 1               
+        elif (lenB - lenA > 0): 
+            while(  lenB != lenA):
+                posB = posB.next
+                lenB -= 1
+        
+        while(posA != posB):
+            posA = posA.next
+            posB = posB.next
+        return posA
+        
+    def getLen(self,headA):
+        if (headA == None):
+            return 0
+        len = 0
+        while (headA) :
+            len += 1
+            headA = headA.next
+        return len
     
-        while temp1 != temp2:
-            if temp1 is not None:
-                temp1 = temp1.next
-            else:
-                temp1 = headB
-            if temp2 is not None:
-                temp2 = temp2.next
-            else:
-                temp2 = headA
-        return temp1
+        # while temp1 != temp2:
+        #     if temp1 is not None:
+        #         temp1 = temp1.next
+        #     else:
+        #         temp1 = headB
+        #     if temp2 is not None:
+        #         temp2 = temp2.next
+        #     else:
+        #         temp2 = headA
+        # return temp1
+        
+        
                 
         
