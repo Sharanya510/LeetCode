7082474326dd8d9a3eb07e7b00f6c89466dcdeff
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-20 23:01:16 -0700

    Time: 45 ms (91.72%), Space: 16.7 MB (18.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,12 +3,51 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[i+m] = nums2[i]
-        nums1.sort()
-            
-            
-                
-# 1   2   3   0   0   0                2  5   6
-# 1   2   2
+        # for i in range(n):
+        #     nums1[i+m] = nums2[i]
+        # nums1.sort()
         
+        p1 = m -1
+        p2 = n -1
+        i = m+n-1
+        while p1 >=0 and p2 >=0:
+                if nums1[p1] < nums2[p2]:
+                    print(nums1)
+                    nums1[i] = nums2[p2]
+                    p2 -= 1
+                    i -= 1
+                elif nums1[p1] == nums2[p2]:
+                    print(nums1)
+                    nums1[i] = nums1[p1]
+                    nums1[i-1] = nums2[p2]
+                    p1 -=1
+                    p2 -=1
+                    i -= 2
+                else:
+                    print(nums1)
+                    nums1[i] = nums1[p1]
+                    p1 -=1
+                    i -= 1
+        # while p1 >=0 and p2 == 0:
+        #     nums1[i] = nums1[p1]
+        #     p1-=1
+        #     i-=1
+        while p2 >=0:
+            nums1[i] = nums2[p2]
+            p2 -= 1
+            i-=1
+        while p1 >=0 :
+            nums1[i] = nums1[p1]
+            p1 -= 1
+            i-=1
+        return nums1
+#         p1          i
+# 4   5   6   0   0   0
+# 1   2   3
+#         p2
+    
+    
+         
+        
+
+            
