1670f0de647e80b7925ec9102533b86e76a2eea0
Sharanya510 <Sharanya.gandla05@gmail.com>
2023-07-24 13:04:19 -0700

    Time: 87 ms (85.66%), Space: 19.9 MB (30.74%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,12 +1,11 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # SORTING
         intervals.sort()
         for i in range(len(intervals) - 1):
             if intervals[i][1] > intervals[i+1][0]:
                 return False
         return True
-        
-        
         
         # BRUTE -- FORCE
 #         def overlap(interval1:List[int], interval2: List[int]) -> bool:
