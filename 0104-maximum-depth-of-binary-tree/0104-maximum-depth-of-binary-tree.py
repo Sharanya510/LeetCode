dc3c5586d879bb462a1dd76991425dbd153f401d
Sharanya <Sharanya.gandla05@gmail.com>
2023-07-29 00:16:30 -0700

    Time: 50 ms (90.14%), Space: 18.7 MB (33.82%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,61 +6,9 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # DEPTH --- FIRST --- SEARCH
-#         if not root:
-#             return 0
-        
-#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
-        # BREADTH --- FIRST --- SEARCH 
-#         if not root:
-#             return 0
-        
-#         level = 0
-#         q = deque([root])
-        
-#         while q:
-#             for i in range(len(q)):
-#                 node = q.popleft()
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-                
-#             level += 1
-            
-#         return level
-
-# ITERATIVE DFS
-        stack = [[root, 1]]
-        res = 0
-        
-        while stack:
-            node, depth = stack.pop()
-            
-            if node:
-                res = max(res, depth)
-                stack.append([node.left, depth + 1])
-                stack.append([node.right, depth + 1])
-        return res
-        
-#  -------- BRUTE--- FORCE -------
-#         length = 0
-#         res_left, res_right = 1, 1
-#         if not root:
-#             return length
-        
-#         if root.left:
-#             res_left += 1
-#         self.maxDepth(root.left)
-#         self.maxDepth(root.right)
-        
-#         if root.right:
-#             res_right += 1
-#         self.maxDepth(root.left)
-#         self.maxDepth(root.right)
-        
-#         length = 1 + max(res_left,res_right )
-#         return length
-        
-        
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height , right_height)+1
