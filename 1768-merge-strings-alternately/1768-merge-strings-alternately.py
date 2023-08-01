e86550ea073aeaff744d1fe8345a3b9909c3486f
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-01 00:45:38 -0700

    Time: 37 ms (94.05%), Space: 16.3 MB (73.18%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,16 +1,30 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        m = len(word1)
-        n = len(word2)
-        i = 0
-        j = 0
+        # ONE POINTER
         res = ""
+        length = max(len(word1), len(word2))
         
-        while i < m or j < n:
-            if i < m:
+        for i in range(length):
+            if i < len(word1):
                 res += word1[i]
-                i += 1
-            if j < n:
-                res += word2[j]
-                j += 1
+            if i < len(word2):
+                res += word2[i]
         return res
+        
+        
+        
+#         TWO POINTERS
+#         m = len(word1)
+#         n = len(word2)
+#         i = 0
+#         j = 0
+#         res = ""
+        
+#         while i < m or j < n:
+#             if i < m:
+#                 res += word1[i]
+#                 i += 1
+#             if j < n:
+#                 res += word2[j]
+#                 j += 1
+#         return res
