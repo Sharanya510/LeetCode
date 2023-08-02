4ec6ea31e168dc787437779c5bf111f461f824de
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-01 21:30:59 -0700

    Time: 58 ms (87.82%), Space: 20.4 MB (85.96%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,19 +6,36 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res = []
-        self.helper(root, res)
-        return res[k-1]
+        stack = []
+        prev = float("-inf")
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            
+            root = stack.pop()
+            if k > 0:
+                k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+                
+        
+        
+        
+        # BRUTE -- FORCE DFS
+#         res = []
+#         self.helper(root, res)
+#         return res[k-1]
     
-    def helper(self, root, res):
-        if not root:
-            return None
-        self.helper(root.left, res)
-        res.append(root.val)
-        self.helper(root.right,res)
-        return res
-        
-        
+#     def helper(self, root, res):
+#         if not root:
+#             return None
+#         self.helper(root.left, res)
+#         res.append(root.val)
+#         self.helper(root.right,res)
+#         return res
         
 # in-order traversal --> left root right
 # 1 null 2 3 4 null null
