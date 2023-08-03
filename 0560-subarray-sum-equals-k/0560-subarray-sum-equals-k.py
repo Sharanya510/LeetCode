624a836ec7b3a3f497eff200f5b0fc334861d268
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-02 21:58:57 -0700

    Time: 268 ms (93.55%), Space: 20.7 MB (12.62%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,23 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sum, count = 0, 0
-        hash = {0:1}
-        for n in nums:
-            sum += n
-            if sum - k in hash:
-                count = count + hash[sum-k]
-            if sum not in hash:
-                hash[sum] = 1
-            else:
-                hash[sum] = hash[sum]+ 1
+        count = 0
+        curr_sum = 0
+        h = defaultdict(int)
+        for num in nums:
+            curr_sum += num
+            if curr_sum == k:
+                count += 1
+            
+            count += h[curr_sum - k]
+            # print("Count is ",count)
+            h[curr_sum] += 1
         return count
-        
-        
+
+# 1   1   1
+# 0
+# 1   2   3
+# count = 2
+# 1:1 
+# 2:1
+# 3:1
+
