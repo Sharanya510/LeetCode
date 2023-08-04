f11ab3de2b4e5b6a8aec550e958390c7e1982727
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-03 17:33:23 -0700

    Time: 183 ms (93.98%), Space: 25.1 MB (48.77%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,17 +6,32 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        res = []
-        output = 0
-        self.helper(root,res)
-        for i in range(len(res)):
-            if res[i] >= low and res[i] <= high:
-                output += res[i]
-        return output
-    def helper(self, root, res):
+        self.res = 0
+        self.helper(root, low, high)
+        return self.res
+    
+    def helper(self, root, low, high):
         if not root:
-            return None
-        self.helper(root.left,res)
-        res.append(root.val)
-        self.helper(root.right,res)
+            return
+        if low <= root.val <= high:
+            self.res += root.val
+        self.helper(root.left, low, high)
+        self.helper(root.right, low, high) 
+        
+# 10  5   15  3   7   n   18
+
+    #     BRUTE -- FORCE
+    #     res = []
+    #     output = 0
+    #     self.helper(root,res)
+    #     for i in range(len(res)):
+    #         if res[i] >= low and res[i] <= high:
+    #             output += res[i]
+    #     return output
+    # def helper(self, root, res):
+    #     if not root:
+    #         return None
+    #     self.helper(root.left,res)
+    #     res.append(root.val)
+    #     self.helper(root.right,res)
         
