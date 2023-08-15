c27e4a1b30d9d0339062b6653087c93ea72cf3bc
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-15 16:01:13 -0700

    Time: 606 ms (82.81%), Space: 30.6 MB (53.30%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -8,4 +8,23 @@ class Solution:
                 curr_sum = nums[i]
             max_sum = max(max_sum, curr_sum)
         return max_sum
+    
+    # to print the subarray itself
+#     start_idx = 0
+#     end_idx = 0
+#     curr_sum = nums[0]
+#     max_sum = nums[0]
+    
+#     for i in range(1, len(nums)):
+#         if nums[i] > curr_sum + nums[i]:
+#             curr_sum = nums[i]
+#             start_idx = i
+#         else:
+#             curr_sum += nums[i]
+        
+#         if curr_sum > max_sum:
+#             max_sum = curr_sum
+#             end_idx = i
+    
+#     return nums[start_idx:end_idx+1]
         
