56b998a5f3458656404397ca8c55723a9fada5a5
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-22 15:56:42 -0700

    Time: 235 ms (41.94%), Space: 17.8 MB (15.55%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -3,34 +3,28 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-        count1 = Counter(word1)
-        count2 = Counter(word2)
-        print(count1)
-        print(count2)
+#         count1 = Counter(word1)
+#         count2 = Counter(word2)
+#         print(count1)
+#         print(count2)
         
-        return count1.keys() == count2.keys() and sorted(count1.values()) == sorted(count2.values())
-        
-        
+#         return count1.keys() == count2.keys() and sorted(count1.values()) == sorted(count2.values())
         
         
-        # word1_hashmap , word2_hashmap = {}, {}
-        # for c in word1:
-        #     if c not in word1_hashmap:
-        #         word1_hashmap[c] = 1
-        #     else:
-        #         word1_hashmap[c] += 1
-        # for c in word2:
-        #     if c not in word2_hashmap:
-        #         word2_hashmap[c] = 1
-        #     else:
-        #         word2_hashmap[c] += 1
-        # print(word1_hashmap.values())
-        # print(word2_hashmap.values())
-        # for value in word1_hashmap.values():
-        #     if value in word2_hashmap.values():
-        #         return True
-        #     else:
-        #         return False
+        
+        
+        word1_hashmap , word2_hashmap = {}, {}
+        for c in word1:
+            if c not in word1_hashmap:
+                word1_hashmap[c] = 1
+            else:
+                word1_hashmap[c] += 1
+        for c in word2:
+            if c not in word2_hashmap:
+                word2_hashmap[c] = 1
+            else:
+                word2_hashmap[c] += 1
+        return word1_hashmap.keys() == word2_hashmap.keys() and sorted(word1_hashmap.values()) == sorted(word2_hashmap.values())
             
 # a   b   b   b   z   c   f
 # a = 1; b = 3; c = 1; f = 1; z = 1
