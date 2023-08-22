6a1d1e8cc9ce95e6d6484f250591383b29ac50eb
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-22 15:55:00 -0700

    Time: 128 ms (86.16%), Space: 17.4 MB (98.91%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,44 @@
+from collections import Counter
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        count1 = Counter(word1)
+        count2 = Counter(word2)
+        print(count1)
+        print(count2)
+        
+        return count1.keys() == count2.keys() and sorted(count1.values()) == sorted(count2.values())
+        
+        
+        
+        
+        # word1_hashmap , word2_hashmap = {}, {}
+        # for c in word1:
+        #     if c not in word1_hashmap:
+        #         word1_hashmap[c] = 1
+        #     else:
+        #         word1_hashmap[c] += 1
+        # for c in word2:
+        #     if c not in word2_hashmap:
+        #         word2_hashmap[c] = 1
+        #     else:
+        #         word2_hashmap[c] += 1
+        # print(word1_hashmap.values())
+        # print(word2_hashmap.values())
+        # for value in word1_hashmap.values():
+        #     if value in word2_hashmap.values():
+        #         return True
+        #     else:
+        #         return False
+            
+# a   b   b   b   z   c   f
+# a = 1; b = 3; c = 1; f = 1; z = 1
+# b   a   b   z   z   c   z
+# a = 1; b = 2; z = 3; c = 1
+# word1 has more letters
+
+# c   a   b   b   b   a
+# a = 2; b = 3; c = 1
+# a   a   b   b   s   s
+# a = 2; b = 2; s = 2
