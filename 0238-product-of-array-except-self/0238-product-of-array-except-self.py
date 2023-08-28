6d5f0d65975f3a26f0b66ad08b656d2ebe38d918
Sharanya <Sharanya.gandla05@gmail.com>
2023-08-28 02:05:59 -0700

    Time: 200 ms (67.19%), Space: 24 MB (70.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,42 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_arr=[1]*len(nums)
-        right_arr=[1]*len(nums)
-             
-        product=1
-        for i in range(len(nums)-1):
-            product=product*nums[i]
-            left_arr[i+1]=product
-        # print(left_arr)
-        
-        product=1
-        
-        for i in range(len(nums)-2,-1,-1):
-            product=product*nums[i+1]
-            right_arr[i]=product
-#         print(right_arr)
-        
-        ans=[]
+        product = 1
+        res = [0]*len(nums)
         
         for i in range(len(nums)):
-            ans.append(left_arr[i]*right_arr[i])
-        return ans
-           
-        # SECOND APPROACH
-#         prefix = 1
-#         for i in range(len(nums)):
-#             res[i] = prefix
-#             prefix *= nums[i]
-            
-#         # print(res)
+            res[i] = product
+            product *= nums[i]
+        # print(res)
         
-#         postfix = 1
-#         for i in range(len(nums) - 1, -1 , -1 ):
-           
-#             res[i] *= postfix
-#             postfix *= nums[i]
-#         # print(res)
-#         return res
-            
-            
-            
+        product = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= product
+            product *= nums[i]
+        # print(res)
+        
+        return res
+
+        
+        
+        
+        
+# p   1   2   3   4
+# 1   1   1   2   6
+#                     p = 24
+#     24  12  8    6
+                
+        
