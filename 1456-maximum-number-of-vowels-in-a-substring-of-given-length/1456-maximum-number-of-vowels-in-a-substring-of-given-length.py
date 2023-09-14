b47a8e51bab4756c24649791508210cf4c6f3793
Sharanya <Sharanya.gandla05@gmail.com>
2023-09-13 22:49:28 -0700

    Time: 125 ms (78.62%), Space: 16.9 MB (99.39%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,33 +1,25 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        count = 0
-        max_count = 0
+        count, max_count = 0, 0
         vowels = {'a', 'e', 'i', 'o', 'u'}
+        
         for i in range(k):
             if s[i] in vowels:
                 count += 1
         max_count = count
-        for i in range(k, len(s)):
-            if s[i] in vowels:
-                count +=1 
-            if s[i-k] in vowels:
-                count -= 1
-            max_count = max(max_count, count)
-        return max_count
         
-#         length = 0
-#         max_length = 0
-#         i = 0
-#         while i < len(s)-k+1:
-#             sub_string = s[i : i+k]
-#             length = self.vowelength(sub_string)
-#             max_length = max(max_length, length)
-#             i += 1 
-#         return max_length
+        for i in range(k, len(s)):
+            if s[i] not in vowels and s[i-k] in vowels:
+                count -= 1
+            elif s[i] in vowels and s[i-k] not in vowels :
+                count += 1
+                
+            max_count = max(count, max_count)
+            
+        return max_count
     
-#     def vowelength(self, s):
-#         length = 0
-#         for char in s:
-#             if char == 'a' or  char =='e' or  char =='i' or  char =='o' or  char =='u':
-#                 length += 1
-#         return length
+    # a   b   c   i   i   i   d   e   f
+    # count = 0
+                
+                
+        
