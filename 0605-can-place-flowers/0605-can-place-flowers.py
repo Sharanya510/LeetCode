df62232d5a198df3aaed8635f554b57e4fd08f98
Sharanya <Sharanya.gandla05@gmail.com>
2023-09-17 14:19:58 -0700

    Time: 152 ms (52.61%), Space: 16.9 MB (17.98%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,52 +1,19 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         for i in range(len(flowerbed)):
-            if(flowerbed[i]==0 and self.helper(flowerbed, i)):
-                flowerbed[i]=1
-                n = n-1
-                if n<=0:
+            if flowerbed[i] == 0 and self.helper(flowerbed, i) :
+                flowerbed[i] = 1
+                n = n - 1
+                if n <= 0:
                     return True
-        return n<=0
-    
+        return n <= 0
+        
     def helper(self, flowerbed, i):
-        if i==0:
-            return len(flowerbed)==1 or flowerbed[i+1]==0
+        if i == 0:
+            return len(flowerbed) == 1 or flowerbed[i+1] == 0
         
-        if i==len(flowerbed)-1:
-            return flowerbed[i-1]==0
+        if i == len(flowerbed) - 1:
+            return flowerbed[i-1] == 0
         
-        return flowerbed[i+1]==0 and flowerbed[i-1]==0
+        return flowerbed[i+1] == 0 and flowerbed[i-1] == 0
         
-        
-        
-        
-        
-        
-        
-        
-#         for i in range(len(flowerbed)-1):
-#             if flowerbed[i] == 1 and flowerbed[i+1] == 0 or  flowerbed[i] == 0 and flowerbed[i+1] == 1:
-#                 continue
-#             if flowerbed[i] == 0 and flowerbed[i+1] == 0:
-#                 if n > 0:
-#                     n -= 1
-#                     continue
-#             if flowerbed[i] == 1 and flowerbed[i+1] == 1:
-#                 if n > 0:
-#                     n -= 1
-#                     continue
-#                 else:
-#                     return False
-#         print(n)
-#         if n == 0:
-#             return True
-        
-#         return False
-                
-                
-# 0   0   1   0   1
-
-                
-                
-
-                
