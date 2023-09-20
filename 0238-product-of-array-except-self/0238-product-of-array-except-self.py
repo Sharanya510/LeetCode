c2165814cdb02049fd9f400978bdbb7ac9ca289a
Sharanya <Sharanya.gandla05@gmail.com>
2023-09-20 01:39:41 -0700

    Time: 187 ms (84.57%), Space: 24.1 MB (48.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,43 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # BRUTE-FORCE
-#         nums1 = [1 for i in range(len(nums))]
-#         nums2 = [1 for i in range(len(nums))]
-        
-#         for i in range(len(nums)-1):
-#             nums1[i+1] = nums1[i] * nums[i]
-#         print(nums1)
-        
-#         for j in range(len(nums)-1, 0, -1):
-#             nums2[len(nums)-j] = nums2[len(nums)-j-1] * nums[j]
-#         print(nums2)
-        
-#         nums3 = [1 for i in range(len(nums))]
-        
-#         for i in range(len(nums)):
-#             nums3[i] = nums1[i] * nums2[len(nums) - 1 - i]
-#         return nums3
-        
-        
-        product = 1
         res = [0]*len(nums)
-        
-        for i in range(len(nums)):
-            res[i] = product
-            product *= nums[i]
-        # print(res)
-        
-        product = 1
+        prefix = 1
+        for i, n in enumerate((nums)):
+            res[i] = prefix 
+            prefix *= n
+        postfix = 1
         for i in range(len(nums)-1, -1, -1):
-            res[i] *= product
-            product *= nums[i]
-        # print(res)
-        
+            res[i] *= postfix 
+            postfix *= nums[i]
         return res
-        
-# p   1   2   3   4
+            
+            
+            
+            
+#     1   2   3   4
 # 1   1   1   2   6
-#                     p = 24
-#     24  12  8    6
-                
-        
+                        # 1
