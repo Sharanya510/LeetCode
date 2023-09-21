975d0d1cedb017d353980f02eb6a239cd3cf53bf
Sharanya <Sharanya.gandla05@gmail.com>
2023-09-20 23:38:34 -0700

    Time: 419 ms (88.47%), Space: 21.7 MB (29.54%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,21 +1,25 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        # BRUTE-FORCE
+        # count = 0
+        # n = len(grid)
+        # for r in range(n):
+        #     for c in range(n):
+        #         match = True
+        #         for i in range(n):
+        #             if grid[r][i] != grid[i][c]:
+        #                 match = False
+        #                 break
+        #         count += int(match)
+        # return count
+        
         count = 0
         n = len(grid)
-        for r in range(n):
-            for c in range(n):
-                match = True
-                for i in range(n):
-                    if grid[r][i] != grid[i][c]:
-                        match = False
-                        break
-                count += int(match)
+        row_counter = collections.Counter(tuple(row) for row in grid)
+        for c in range(n):
+            col = [grid[i][c] for i in range(n)]
+            count += row_counter[tuple(col)]
         return count
-        
-        
-        
-        
-        
         
         
 #         rows = len(grid)
