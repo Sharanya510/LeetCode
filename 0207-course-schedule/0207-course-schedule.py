480e45f8f9c35f7e6af8f5d143e9ea886f1e508e
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-05 18:03:44 -0700

    Time: 94 ms (81.44%), Space: 18 MB (72.60%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+        indegree=[0]*numCourses
+        course_map=defaultdict(list)
+        
+        for preq in prerequisites:
+            indegree[preq[0]]+=1
+            course_map[preq[1]].append(preq[0])
+        print(indegree)
+        print(course_map)
+        
+        q=deque()
+        
+        for i in range(len(indegree)):
+            if indegree[i]==0:
+                q.append(i)
+        print(q)
+        while q:
+            course=q.popleft()
+            for child in course_map[course]:
+                indegree[child]-=1
+                if indegree[child]==0:
+                    q.append(child)
+        
+        for i in range(len(indegree)):
+            if indegree[i]!=0:
+                return False
+        return True
+    
+    
+    
+        
