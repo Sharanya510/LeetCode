007dd9ede9727e4bfd779f75469e26055607cc14
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-09 17:11:45 -0700

    Time: 54 ms (72.07%), Space: 16.2 MB (94.36%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,37 +1,44 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
         m=len(grid)
         n=len(grid[0])
-        minutes=0
-        fresh=0
         queue=deque()
+        fresh=0
+        
         for i in range(m):
             for j in range(n):
+                if grid[i][j]==1:
+                    fresh+=1
                 if grid[i][j]==2:
                     queue.append([i,j])
-                elif grid[i][j]==1:
-                    fresh+=1
-        if fresh==0:
+        if fresh == 0:
             return 0
-        directions=[[0,1],[0,-1],[-1,0],[1,0]]           
+        directions=[[0,1],[-1,0],[1,0],[0,-1]]
+        mins=0
+        # visited=set()
+        
         while queue:
             size=len(queue)
-            for i in range(size):
-                node=queue.popleft()
-                x=node[0]
-                y=node[1]
+            for val in range(size):
+                i,j=queue.popleft()
                 for dirs in directions:
-                    new_x=x+dirs[0]
-                    new_y=y+dirs[1]
-                    if new_x>=0 and new_x<len(grid) and new_y>=0 and new_y<len(grid[0]) and grid[new_x][new_y]==1 and grid[new_x][new_y]!=0:
+                    new_x=dirs[0]+i
+                    new_y=dirs[1]+j
+                    if new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]!=2 and grid[new_x][new_y]!=0:
                         grid[new_x][new_y]=2
-                        fresh-=1
                         queue.append([new_x,new_y])
-            minutes+=1
+                        fresh-=1
+            mins+=1
+            
         if fresh==0:
-            return minutes-1
+            return mins-1
         return -1
+    
+                
                 
             
-        
+                    
+                    
         
