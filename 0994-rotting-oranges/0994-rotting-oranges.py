b1293fb6abd2230e6d67ad704f88b425546983bd
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-09 17:12:26 -0700

    Time: 55 ms (66.01%), Space: 16.5 MB (14.94%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -17,16 +17,17 @@ class Solution:
             return 0
         directions=[[0,1],[-1,0],[1,0],[0,-1]]
         mins=0
-        # visited=set()
+        visited=set()
         
         while queue:
             size=len(queue)
             for val in range(size):
                 i,j=queue.popleft()
+                visited.add((i,j))
                 for dirs in directions:
                     new_x=dirs[0]+i
                     new_y=dirs[1]+j
-                    if new_x>=0 and new_x<m and new_y>=0 and new_y<n and grid[new_x][new_y]!=2 and grid[new_x][new_y]!=0:
+                    if new_x>=0 and new_x<m and new_y>=0 and new_y<n and (new_x, new_y) not in visited and grid[new_x][new_y]!=2 and grid[new_x][new_y]!=0:
                         grid[new_x][new_y]=2
                         queue.append([new_x,new_y])
                         fresh-=1
