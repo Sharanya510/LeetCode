a32683577ad1468036b7cfe5f13d0b5d14f4a668
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-13 14:00:27 -0700

    Time: 292 ms (16.91%), Space: 17.9 MB (68.24%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,32 +1,37 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pacific, atlantic = set(), set()
+        if not heights:
+            return []
+        row = len(heights)
+        col = len(heights[0])
+        pacific_queue = deque()
+        atlantic_queue = deque()
         
-        def dfs(r, c, visit, prevHeight):
-            if ((r,c) in visit or
-                r < 0 or c < 0 or r == ROWS or c == COLS or
-                heights[r][c] < prevHeight ):
-                return
-            visit.add((r,c))
-            dfs(r+1, c, visit, heights[r][c])
-            dfs(r-1, c, visit, heights[r][c])
-            dfs(r, c+1, visit, heights[r][c])
-            dfs(r, c-1, visit, heights[r][c])
-                
-        for c in range(COLS):
-            dfs(0, c, pacific, heights[0][c])
-            dfs(ROWS-1, c, atlantic, heights[ROWS-1][c])
+        for i in range(row):
+            pacific_queue.append((i, 0))
+            atlantic_queue.append((i, col - 1))
+        for j in range(col):
+            pacific_queue.append((0, j))
+            atlantic_queue.append((row-1, j))
         
-        for r in range(ROWS):
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, COLS-1, atlantic, heights[r][COLS-1])
-            
-        res = []
+        pacific_bfs = self.bfs(pacific_queue, heights)
+        atlantic_bfs = self.bfs(atlantic_queue, heights)
+        return list(pacific_bfs.intersection(atlantic_bfs))
+    
+    
+    def bfs(self, queue, heights):
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        visited=set()
+        while queue:
+            x, y = queue.popleft()
+            visited.add((x, y))
+            for dirs in directions:
+                new_x = dirs[0] + x
+                new_y = dirs[1] + y
+                if 0<=new_x< len(heights) and 0<=new_y < len(heights[0]) and (new_x, new_y) not in visited and heights[new_x][new_y] >= heights[x][y]:
+                    queue.append((new_x, new_y))
+                       
+        return visited
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r,c) in pacific and (r,c) in atlantic:
-                    res.append([r,c])
-        return res
+        
         
