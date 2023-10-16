f17478f2677684e94212e733008f64856fd44c4b
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-15 19:20:15 -0700

    Time: 133 ms (78.03%), Space: 17.6 MB (92.43%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -11,12 +11,16 @@ class Solution:
         col = len(board[0])
         visited = set()
         
+        # below for loop is to check for "0" in the borders
+#         if there is any zero in the border, then add it to visited and queue
         for i in range(row):
             for j in range(col):
                 if (i == 0 or i == row -1 or j == 0 or j == col - 1) and board[i][j] == "O" :
                     visited.add((i,j))
                     queue.append([i,j])
             
+#      for the values added prior, if there is any connected zero, then you skip
+# for the values added prior, if there are no connecting zeros, which means for a value if there is only X then add those to visited and then loop over that
         while queue:
             x, y = queue.popleft()
             directions = [[0,1], [0, -1], [1,0], [-1, 0]]
@@ -27,6 +31,7 @@ class Solution:
                     visited.add((new_x, new_y))
                     queue.append([new_x,new_y])
         
+#         values not in visited should be marked as X
         for i in range(row):
             for j in range(col):
                 if (i,j) not in visited and board[i][j] == "O":
