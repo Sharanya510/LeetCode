1bd2a47a4d76f272c91b357ef5d80ed405b2efb8
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-27 12:39:47 -0700

    Time: 111 ms (88.97%), Space: 16.7 MB (54.57%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,29 +1,39 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
+        # BRUTE-FORCE
+        # self.big = big
+        # self.medium = medium
+        # self.small = small
+        
+        self.empty = [big, medium, small]
         
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big >= 1:
-                self.big -= 1
-                return True
-            elif self.big == 0:
-                return False
-        elif carType == 2:
-            if self.medium >= 1:
-                self.medium -= 1
-                return True
-            elif self.medium == 0:
-                return False
-        elif carType == 3:
-            if self.small >= 1:
-                self.small -= 1
-                return True
-            elif self.small == 0:
-                return False
+        
+        if self.empty[carType - 1] > 0:
+            self.empty[carType - 1] -= 1
+            return True
+        return False
+        
+        # BRUTE--FORCE
+        # if carType == 1:
+        #     if self.big >= 1:
+        #         self.big -= 1
+        #         return True
+        #     elif self.big == 0:
+        #         return False
+        # elif carType == 2:
+        #     if self.medium >= 1:
+        #         self.medium -= 1
+        #         return True
+        #     elif self.medium == 0:
+        #         return False
+        # elif carType == 3:
+        #     if self.small >= 1:
+        #         self.small -= 1
+        #         return True
+        #     elif self.small == 0:
+        #         return False
             
 
 
