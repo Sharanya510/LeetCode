b8b40ce98d2fa77d519a3e45fb41d91aafde0c74
Sharanya <Sharanya.gandla05@gmail.com>
2023-10-31 22:15:41 -0700

    Time: 74 ms (58.88%), Space: 23.7 MB (41.46%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,18 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    count = 0
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
+        self.count = 0
+        return self.helper(root) + 1
+    
+    def helper(self, root):
+        if not root:
+            return 0
         if root.left:
             self.count += 1
-            self.countNodes(root.left)
-            
+            self.helper(root.left)
         if root.right:
             self.count += 1
-            self.countNodes(root.right)
-        return self.count +1 
+            self.helper(root.right)
+        return self.count
+        
+        
+        
+        
+#         if root.left:
+#             self.count += 1
+#             self.countNodes(root.left)
+            
+#         if root.right:
+#             self.count += 1
+#             self.countNodes(root.right)
+#         return self.count +1 
             
         
