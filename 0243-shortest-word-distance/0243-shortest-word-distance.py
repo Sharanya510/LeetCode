eabda46c2feb587f71eae0d9c0980a533e0027ba
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-01 14:48:22 -0700

    Time: 76 ms (37.02%), Space: 20.2 MB (33.09%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.py
+++ 0243-shortest-word-distance/0243-shortest-word-distance.py
@@ -1,28 +1,29 @@
 class Solution:
     def shortestDistance(self, words: List[str], word1: str, word2: str) -> int:
-        shortest_distance = float("inf")
-        for i in range(len(words)):
-            if words[i]==word1:
-                for j in range(len(words)):
-                    if words[j]==word2:
-                        if j>i:
-                            distance=j-i
-                        else:
-                            distance=i-j
-                        shortest_distance=min(shortest_distance,distance)
+        first_index, second_index = -1, -1
+        shortest_distance = len(words)
+        for index, word in enumerate(words):
+            if word == word1:
+                first_index = index
+            if word == word2:
+                second_index = index
+                
+            if first_index != -1 and second_index != -1:
+                shortest_distance = min(shortest_distance, abs(first_index - second_index))
         return shortest_distance
         
-#         first_index, second_index = 0, 0
-#         for index, word in enumerate(wordsDict):
-#             if word == word1:
-#                 first_index = index
-#             if word == word2:
-#                 second_index = index
-                
-#             if second_index > first_index:
-#                 distance = second_index - first_index
-#             else:
-#                 distance = first_index - second_index
-            
-#         shortest_distance = min(distance, shortest_distance)
-#         return shortest_distance
+        
+        # BRUTE -- FORCE   
+        # shortest_distance = float("inf")
+        # for i in range(len(words)):
+        #     if words[i]==word1:
+        #         for j in range(len(words)):
+        #             if words[j]==word2:
+        #                 if j>i:
+        #                     distance=j-i
+        #                 else:
+        #                     distance=i-j
+        #                 shortest_distance=min(shortest_distance,distance)
+        # return shortest_distance
+        
+        
