7ed63c53b19947cf70d52eb6e6eb4eb1e3331416
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-01 13:47:13 -0700

    Time: 46 ms (19.36%), Space: 16.2 MB (83.24%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.py
+++ 1023-camelcase-matching/1023-camelcase-matching.py
@@ -1,14 +1,18 @@
 class Solution:
     def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
-        return [True if self.match(pattern, s) else False for s in queries]
-    
-    def match(self,p, q):
-            i = 0
-            for j, c in enumerate(q):
-                if i < len(p) and p[i] == q[j]: i += 1
-                elif q[j].isupper(): return False
-            return i == len(p)
-        
-        
+        res = []
+        for query in queries:
+            if self.helper(pattern, query):
+                res.append(True)
+            else:
+                res.append(False)
+        return res
             
-            
+    def helper(self, pattern, query):
+        i = 0
+        for index, char in enumerate(query):
+            if i < len(pattern) and pattern[i] == query[index]:
+                i += 1
+            elif char.isupper():
+                return False
+        return i == len(pattern)
