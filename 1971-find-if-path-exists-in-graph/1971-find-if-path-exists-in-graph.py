6e4ae82673bf0ef75e37dc79a9c04e74f0587bf5
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-03 14:44:49 -0700

    Time: 1885 ms (29.78%), Space: 317.9 MB (9.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,21 +1,19 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        queue = deque()
-        queue.append(source)
-        adj_list = defaultdict(list)
-        for a,b in edges:
-            adj_list[a].append(b)
-            adj_list[b].append(a)
-        
         visited = set()
-        visited.add(source)
-        while queue:
-            parent = queue.popleft()
-            if parent == destination:
-                return True
-            for child in adj_list[parent]:
-                if child not in visited:
-                    visited.add(child)
-                    queue.append(child)
+        adj_list = defaultdict(list)
+        for edge in edges:
+            adj_list[edge[0]].append(edge[1])
+            adj_list[edge[1]].append(edge[0])
+        return self.helper(source, destination, adj_list, visited)
+    
+    def helper(self, source, destination, adj_list, visited):
+        if source == destination:
+            return True
+        if source not in visited:
+            visited.add(source)
+            for child in adj_list[source]:
+                if self.helper(child, destination, adj_list, visited):
+                    return True
         return False
-        
+                
