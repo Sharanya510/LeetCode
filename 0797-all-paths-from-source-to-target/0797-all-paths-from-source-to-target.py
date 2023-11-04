f58e0037baa9a27c3455ed5ec970725025061a3c
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-03 23:36:11 -0700

    Time: 96 ms (10.93%), Space: 17.9 MB (95.60%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,20 +1,25 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        queue = deque()
-        queue.append([0])
         adj_list = {}
-        res = []
         n = len(graph)
         for index, edge in enumerate(graph):
             adj_list[index] = edge
-            # print(adj_list)
-        while queue:
-            parent = queue.popleft()
-            if parent[-1] == n-1:
-                res.append(parent)
-            for child in adj_list[parent[-1]]:
-                queue.append(parent+[child])
-                # print(queue)
+        visited=set()
+        res=[]
+        temp=[]
+        self.helper(0, adj_list,res,temp,n)
         return res
-                
-                
+    
+    def helper(self,source,adj_list,res,temp,n):
+        temp.append(source)
+        if source==n-1:
+            res.append(list(temp))
+            temp.pop()
+            return
+        for child in adj_list[source]:
+            self.helper(child,adj_list,res,temp,n)
+        temp.pop()
+        
+        
+        
+        
