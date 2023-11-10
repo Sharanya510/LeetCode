9748f16604ee0de2d82aafad42d45eed312ac1ea
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-09 18:08:55 -0800

    Time: 36 ms (71.87%), Space: 16.4 MB (44.01%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -5,19 +5,16 @@ class MyStack:
         self.queue2 = []
         
     def push(self, x: int) -> None:
-        # if len(self.queue1) == 0:
-        #     self.queue1.append(x)
         self.queue1.append(x)
-        print(self.queue1)
         self.last_element = self.queue1[-1]
 
     def pop(self) -> int:
-        if len(self.queue2) == 0:
-            while self.queue1:
-                self.queue2.append(self.queue1.pop(0))
-        else:
-            while self.queue1:
-                self.queue2.append(self.queue1.pop(0))
+        # if len(self.queue2) == 0:
+        #     while self.queue1:
+        #         self.queue2.append(self.queue1.pop(0))
+        # else:
+        while self.queue1:
+            self.queue2.append(self.queue1.pop(0))
         return self.queue2.pop()
 
     def top(self) -> int:
