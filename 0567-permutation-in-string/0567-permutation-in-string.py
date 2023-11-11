64e80446ca9acf51e79445e801a2aee641950093
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-10 20:23:36 -0800

    Time: 59 ms (88.53%), Space: 16.4 MB (58.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,25 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        start = end = 0
-        memo = collections.defaultdict(int)
-        for s in s1:
-            memo[s] += 1
-        print(memo)
-        counter = len(memo)
-        while end < len(s2):
-            c = s2[end]
-            if c in memo:
-                memo[c] -= 1
-                if memo[c] == 0:
-                    counter -= 1
-            end += 1
-            while counter == 0:
-                t = s2[start]
-                if t in memo:
-                    memo[t] += 1
-                    if memo[t] >0 :
-                        counter += 1
-                if end - start == len(s1):
+        start=end=0
+        s1_map=defaultdict(int)
+        for c in s1:
+            s1_map[c]+=1
+        counter=len(s1_map)
+        while end<len(s2):
+            char=s2[end]
+            if char in s1_map:
+                s1_map[char]-=1
+                if s1_map[char]==0:
+                    counter-=1
+            end+=1
+            while counter==0:
+                left_char=s2[start]
+                if left_char in s1_map:
+                    s1_map[left_char]+=1
+                    if s1_map[left_char]>0:
+                        counter+=1
+                if end-start==len(s1):
                     return True
-                start += 1
+                start+=1
         return False
+    
+        
