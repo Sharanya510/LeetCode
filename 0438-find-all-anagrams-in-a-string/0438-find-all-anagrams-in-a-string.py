8621b9950da1e17fb295581ba826b2f42bdbbfae
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-14 22:52:21 -0800

    Time: 118 ms (50.13%), Space: 17.5 MB (11.25%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        p_hashmap = Counter(p)
+        counter = len(p_hashmap)
+        start, end = 0, 0
+        while(end < len(s)):
+            char = s[end]
+            if char in p_hashmap:
+                p_hashmap[char] -= 1
+                if p_hashmap[char] == 0:
+                    counter -= 1
+            while(counter == 0):
+                if end - start + 1 == len(p):
+                    res.append(start)
+                ch = s[start]
+                if ch in p_hashmap:
+                    p_hashmap[ch] += 1
+                    if p_hashmap[ch] == 1:
+                        counter += 1
+                start +=1
+            end += 1
+        return res
+        
+        
