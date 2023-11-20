7fffc32dd1fc2e6d51cf1de0d22a09131ad659d2
Sharanya <Sharanya.gandla05@gmail.com>
2023-11-19 22:38:59 -0800

    Time: 279 ms (19.75%), Space: 18 MB (36.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,34 +4,29 @@ class Solution:
             return []
         row = len(heights)
         col = len(heights[0])
-        pacific_queue = deque()
-        atlantic_queue = deque()
-        
+        pacific_ocean = deque()
+        atlantic_ocean = deque()
         for i in range(row):
-            pacific_queue.append((i, 0))
-            atlantic_queue.append((i, col - 1))
+            pacific_ocean.append((i, 0))
+            atlantic_ocean.append((i, col - 1))
         for j in range(col):
-            pacific_queue.append((0, j))
-            atlantic_queue.append((row-1, j))
-        
-        pacific_bfs = self.bfs(pacific_queue, heights)
-        atlantic_bfs = self.bfs(atlantic_queue, heights)
+            pacific_ocean.append((0, j))
+            atlantic_ocean.append((row - 1, j))
+        pacific_bfs = self.bfs(pacific_ocean, heights)
+        atlantic_bfs = self.bfs(atlantic_ocean, heights)
         return list(pacific_bfs.intersection(atlantic_bfs))
     
-    
     def bfs(self, queue, heights):
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        visited=set()
+        visited = set()
+        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
         while queue:
             x, y = queue.popleft()
             visited.add((x, y))
             for dirs in directions:
-                new_x = dirs[0] + x
-                new_y = dirs[1] + y
-                if 0<=new_x< len(heights) and 0<=new_y < len(heights[0]) and (new_x, new_y) not in visited and heights[new_x][new_y] >= heights[x][y]:
+                new_x = x + dirs[0]
+                new_y = y + dirs[1]
+                if new_x >= 0 and new_x < len(heights) and new_y >= 0 and new_y < len(heights[0]) and heights[new_x][new_y] >= heights[x][y] and (new_x, new_y) not in visited:
                     queue.append((new_x, new_y))
-                       
         return visited
-        
-        
-        
+                
+                
