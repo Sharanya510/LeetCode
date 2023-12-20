d2347571ccae29aa886c227dea23099e30ef5ad8
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-19 20:31:19 -0800

    Time: 108 ms (50.56%), Space: 17.4 MB (69.98%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,24 +6,6 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-# USING DFS
-#         return self.dfs(root, subRoot)
-    
-#     def dfs(self, root, subRoot):
-#         if root is None:
-#             return False
-#         elif self.is_identical(root, subRoot):
-#             return True
-#         return self.dfs(root.left, subRoot) or self.dfs(root.right, subRoot)
-    
-#     def is_identical(self, node1, node2):
-#         if node1 is None or node2 is None:
-#             return node1 is None and node2 is None
-        
-#         return ((node1.val == node2.val) and self.is_identical(node1.left, node2.left) and self.is_identical(node1.right, node2.right))
-    
-        
-        # USING RECURSION
         if not subRoot:
             return True
         if not root:
@@ -31,7 +13,6 @@ class Solution:
         if self.helper(root, subRoot):
             return True
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        
     
     def helper(self, p, q):
         if not p and not q:
