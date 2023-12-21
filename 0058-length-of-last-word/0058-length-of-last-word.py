c530260fe2442b3e277a7a8491cb48ba0923bba2
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-20 21:48:03 -0800

    Time: 24 ms (99.53%), Space: 17.2 MB (9.27%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,19 +1,13 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        new_string = s.strip()
-        # print(s)
-        # print(s.strip())
-        res = []
-        out = ""
-        for i in range(len(new_string)):
-            if new_string[i] == " " :
-                res.append(out)
-                out = ""
-            elif i == len(new_string) - 1:
-                out += new_string[i]
-                res.append(out)
-            else:
-                out += new_string[i]
-        return len(res[-1])
+        end_pointer = len(s)
+        length = 0
+        while end_pointer > 0:
+            end_pointer -= 1
+            if s[end_pointer] != " ":
+                length += 1
+            elif length > 0:
+                return length
+        return length
                 
         
