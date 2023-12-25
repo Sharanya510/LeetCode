14cb7856ae65641fae071aabd0d5e791e1ffdd7f
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-24 17:14:12 -0800

    Time: 33 ms (88.29%), Space: 17.4 MB (5.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,48 +7,10 @@ class Solution:
             if c in stack_map:
                 my_stack.append(stack_map[c])
             elif my_stack:
-                if my_stack[-1] == c :
+                if my_stack[-1] == c:
                     my_stack.pop()
                 else:
                     return False
             else:
                 return False
         return len(my_stack) == 0
-        
-        
-        # BRUTE--FORCE
-#         my_stack = []
-#         for c in s:
-#             if c == '(':
-#                 my_stack.append(')')
-#             elif c == '[':
-#                 my_stack.append(']')
-#             elif c == '{':
-#                 my_stack.append('}')
-#             elif c == ')':
-#                 if my_stack:
-#                     if my_stack[-1] == ')':
-#                         my_stack.pop()
-#                     else:
-#                         return False
-#                 else:
-#                     return False
-#             elif c == ']':
-#                 if my_stack:
-#                     if my_stack[-1] == ']':
-#                         my_stack.pop()
-#                     else:
-#                         return False
-#                 else:
-#                     return False
-#             elif c == '}':
-#                 if my_stack:
-#                     if my_stack[-1] == '}':
-#                         my_stack.pop()
-#                     else:
-#                         return False
-#                 else:
-#                     return False
-                
-#         return len(my_stack) == 0
-        
