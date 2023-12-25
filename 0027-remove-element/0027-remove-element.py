a7b7c5a9618ee5871c1e20806f1498aae92d48c5
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-24 17:31:40 -0800

    Time: 37 ms (77.75%), Space: 17.3 MB (5.23%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -3,7 +3,7 @@ class Solution:
         if not nums:
             return 0
         i = 0
-        while i <= len(nums) - 1:
+        while i < len(nums):
             if nums[i] == val:
                 del nums[i]
             else:
