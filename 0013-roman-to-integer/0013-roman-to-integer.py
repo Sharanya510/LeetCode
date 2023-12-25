faef64d19643bac36335e46b46829691db5f441e
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-24 16:46:03 -0800

    Time: 49 ms (70.05%), Space: 17.2 MB (5.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = { 'I' : 1, 'V': 5, 'X': 10 , 'L': 50, 'C': 100,'D': 500, 'M': 1000}
+        d = {'I' : 1, 'V': 5, 'X': 10 , 'L': 50, 'C': 100,'D': 500, 'M': 1000}
         
         res = 0
         
@@ -10,4 +10,3 @@ class Solution:
             else:
                 res += d[s[i]]
         return res
-        
