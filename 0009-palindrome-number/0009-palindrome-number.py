d9f2d652ca5a6ef2afd9b53ac52b08e654ed31d6
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-24 16:21:24 -0800

    Time: 69 ms (24.89%), Space: 17.1 MB (5.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,28 +3,10 @@ class Solution:
         if x < 0:
             return False
         
-        reversed_num = 0
+        rev_num = 0
         temp = x
-        
         while temp != 0:
-            digit = temp % 10
-            reversed_num = reversed_num * 10 + digit
-            temp //=10
-        return reversed_num == x
-            
-#          121
-#         temp = 121
-#         digit = 121 % 10 = 1
-#         rev 1
-#         temp = 121 // 10 = 12
-        
-#         temp = 12
-#         digit = 2
-#         rev = 10 + 2 = 12
-#         temp = 1
-        
-#         temp = 1
-#         digit = 1
-#         rev = 121
-#         temp = 0
-        
+            remainder = temp % 10
+            rev_num = rev_num * 10 + remainder
+            temp = temp // 10
+        return rev_num == x
