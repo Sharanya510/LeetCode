c81d425499b72940e18deb2960d32c3a8b535ddc
Sharanya <Sharanya.gandla05@gmail.com>
2023-12-26 18:55:42 -0800

    Time: 54 ms (16.04%), Space: 18.4 MB (5.37%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for num in range(1, n+1):
+            divisible_by_3 = (num % 3 == 0)
+            divisible_by_5 = (num % 5 == 0)
+            
+            if divisible_by_3 and divisible_by_5:
+                res.append("FizzBuzz")
+            elif divisible_by_3:
+                res.append("Fizz")
+            elif divisible_by_5:
+                res.append("Buzz")
+            else:
+                res.append(str(num))
+        return res
