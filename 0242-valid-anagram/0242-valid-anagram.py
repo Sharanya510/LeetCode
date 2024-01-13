59acf3f37144040fe6f330c8faabce23aed1741a
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-12 22:37:27 -0800

    Time: 49 ms (73.65%), Space: 17.6 MB (33.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        s_hashmap, t_hashmap = {}, {}
+        for char1 in s:
+            if char1 not in s_hashmap:
+                s_hashmap[char1] = 1
+            else:
+                s_hashmap[char1] += 1
+        for char2 in t:
+            if char2 not in t_hashmap:
+                t_hashmap[char2] = 1
+            else:
+                t_hashmap[char2] += 1
+        return s_hashmap == t_hashmap
+        
+        
+# APPROACH 1 SORTING TWO WORDS
+# TIME COMPLEXITY --> O(logn)
+# SPACE COMPLEXITY --> O(1)
+        # return sorted(s) == sorted(t)
         
