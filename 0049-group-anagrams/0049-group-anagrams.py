fc05ddc7508a45f1ef26e811a3700a53baad965e
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-12 23:14:14 -0800

    Time: 86 ms (89.22%), Space: 23.1 MB (8.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
-            res[tuple(sorted(s))].append(s)
-        return res.values()
-        
-        
-        
+        # APPROACH 1 --> SORTING
+        # TIME COMPLEXITY --> O(NM LOGM) 
+        # N --> LENGTH OF THE ARRAY
+        # M --> MAX SIZE OF THE WORD IN THE ARRAY
+        # SPACE COMPLEXITY --> O(NM)
         # res = defaultdict(list)
-        # for word in strs:
-        #     count = [0]* 26
-        #     for c in word:
-        #         count[ord(c) - ord('a')] += 1
-        #     res[tuple(count)].append(word)
+        # for s in strs:
+        #     res[tuple(sorted(s))].append(s)
         # return res.values()
+        
+        
+        # APPROACH 2 --> COUNTER
+        # TIME COMPLEXITY --> O(NM)
+        # N --. LENGTH OF THE ARRAY
+        # M --> MAX LENGTH OF A WORD IN THE ARRAY
+        # SPACE COMPLEXITY --> O(NM)
+        res = defaultdict(list)
+        for word in strs:
+            count = [0]* 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(word)
+        return res.values()
                 
