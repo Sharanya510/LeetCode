f6c6d4c6b1f58585c44000115a80b7b46b233aec
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-12 23:08:07 -0800

    Time: 92 ms (74.27%), Space: 23.2 MB (8.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        output_list = defaultdict(list)
-        for string in strs:
-            char_count = [0] * 26
-            for char in string:
-                char_count[ord(char) - ord('a')] += 1
-            output_list[tuple(char_count)].append(string)
-        return output_list.values()
-    
-    # ["eat","tea","tan","ate","nat","bat"]
-    # output_list = []
-    # count = [0]*26
-    # eat --> count = [1 0 0 ]
-    
-    
+        res = defaultdict(list)
+        
+        for word in strs:
+            count = [0]* 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(word)
+        return res.values()
+                
