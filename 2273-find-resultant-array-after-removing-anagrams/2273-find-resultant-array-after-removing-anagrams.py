65dfeb04b8201d2119bac4c2ba7b04556d9883a4
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-14 00:47:12 -0800

    Time: 54 ms (83.93%), Space: 17.4 MB (20.55%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,18 +1,24 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        res = []
-        anagrams = {}
+        ans=[words[0]]
+        for i in range(1,len(words)):
+            if(sorted(words[i-1])!=sorted(words[i])):
+                ans.append(words[i])
+        
+        return ans
+#         res = []
+#         anagrams = {}
 
-        for i in range(len(words)):
-          word = words[i]
-          counter = [0]*26
-          for c in word:
-            counter[ord(c)-ord('a')] += 1
+#         for i in range(len(words)):
+#           word = words[i]
+#           counter = [0]*26
+#           for c in word:
+#             counter[ord(c)-ord('a')] += 1
 
-          if tuple(counter) not in anagrams:
-            res.append(word)
-          else:
-            if anagrams[tuple(counter)] != words[i-1]:
-              res.append(word)
-          anagrams[tuple(counter)] = word
-        return res
+#           if tuple(counter) not in anagrams:
+#             res.append(word)
+#           else:
+#             if anagrams[tuple(counter)] != words[i-1]:
+#               res.append(word)
+#           anagrams[tuple(counter)] = word
+#         return res
