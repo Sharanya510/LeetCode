5c6c78d3f2191f091213770ad82cbf1fa56e0a9a
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-14 11:41:40 -0800

    Time: 53 ms (42.28%), Space: 17.9 MB (33.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,47 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-        while l < r :
-            while l < r and not self.isalphanumer(s[l]):
-                l += 1
-            while l < r and not self.isalphanumer(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+        if not s:
+            return True
+        s=s.strip()
+        i=0
+        j=len(s)-1
+        while i<=j:
+            while i<=j and not s[i].isalnum():
+                i+=1
+            while j>=i and not s[j].isalnum():
+                j-=1
+            if i<=j and s[i].isnumeric() and s[j].isnumeric():
+                if s[i]!=s[j]:
+                    return False
+            if i<=j and s[i].lower()!=s[j].lower():
                 return False
-            l += 1
-            r -= 1
+            else:
+                i+=1
+                j-=1
         return True
-    def isalphanumer(self, c):
-        return (
-        ord("A") <= ord(c) <= ord("Z") or
-        ord("a") <= ord(c) <= ord("z") or
-        ord("0") <= ord(c) <= ord("9"))
-        
-        
-        
-        # BRUTE--FORCE o(n)
-#         res = ""
-#         for c in s:
-#             if c.isalnum():
-#                 res += c
-#             else:
-#                 continue
-#         # print(res)
-#         rev_res = ""
-#         for i in range(len(res)-1, -1, -1):
-#             if res[i].isupper():
-#                 rev_res += res[i].lower()
-#             else:
-#                 rev_res += res[i]
-#         print(rev_res)
-        
-#         new_res = rev_res[::-1]
-#         print(new_res)
-#         return rev_res == new_res
-                
-                
-                
-                
-            
-            
-        
