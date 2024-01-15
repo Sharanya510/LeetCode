b7c4f147c0d5d2c1e423d3c330d2eb6e3ef7635d
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-14 22:13:56 -0800

    Time: 35 ms (83.85%), Space: 17.4 MB (17.73%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,22 +5,43 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        temp = head
-        length = 0
-        while temp:
-            length += 1
-            temp = temp.next
-        
-        curr = head
-        k = length - n
-        if k == 0:
-            return curr.next
-        while k > 1:
-            curr = curr.next
-            k -= 1
-        
-        if curr.next:
-            curr.next = curr.next.next
-            return head
-        else:
+        # APPROACH --> ONE PASS ALGORITHM
+        # TIME COMPLEXITY --> O(N)
+        # SPACE COMPLEXITY --> O(1)
+        p1 = head
+        p2 = head
+        if not head.next:
             return None
+        while n:
+            p1 = p1.next
+            n -= 1
+        if p1 is None:
+            return p2.next
+        while p1 and p1.next:
+            p1 = p1.next
+            p2 = p2.next
+        if p2.next:
+            p2.next = p2.next.next
+        return head
+        # APPROACH --> TWO PASS ALGO
+        # TIME COMPLEXITY --> O(N)
+        # SPACE COMPLEXITY --> O(1)
+#         temp = head
+#         length = 0
+#         while temp:
+#             length += 1
+#             temp = temp.next
+        
+#         curr = head
+#         k = length - n
+#         if k == 0:
+#             return curr.next
+#         while k > 1:
+#             curr = curr.next
+#             k -= 1
+        
+#         if curr.next:
+#             curr.next = curr.next.next
+#             return head
+#         else:
+#             return None
