61a13a1cc0b8b08a9421d5da41765c7c295870bb
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 13:22:00 -0800

    Time: 42 ms (79.39%), Space: 19.3 MB (30.75%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,11 +6,31 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        min_value = float("-inf")
-        max_value = float("inf")
-        return self.helper(root, min_value, max_value)
-    
-    def helper(self, root, min_val, max_val):
         if not root:
             return True
-        return (root.val > min_val and root.val < max_val and self.helper(root.left, min_val, root.val) and self.helper(root.right, root.val , max_val))
+        min_value = float("-inf")
+        max_value = float("inf")
+        
+        my_stack = [(root, min_value, max_value)]
+        while my_stack:
+            root, lower, upper = my_stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            my_stack.append((root.left, lower, val))
+            my_stack.append((root.right, val, upper))
+        return True
+    
+        # APPROACH --> RECURSION
+        # TIME COMPLEXITY --> O(N)
+        # SPACE COMPLEXIXTY --> O(N)
+#         min_value = float("-inf")
+#         max_value = float("inf")
+#         return self.helper(root, min_value, max_value)
+    
+#     def helper(self, root, min_val, max_val):
+#         if not root:
+#             return True
+#         return (root.val > min_val and root.val < max_val and self.helper(root.left, min_val, root.val) and self.helper(root.right, root.val , max_val))
