63477e63313f5fcf50631d2a534c82cf0e840395
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 15:21:54 -0800

    Time: 54 ms (46.12%), Space: 20.2 MB (94.99%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,50 +6,14 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.k = k
-        self.ans = None
-        self.inorder(root)
-        return self.ans
+        res = []
+        self.inorder(root, res)
+        return res[k-1]
     
-    def inorder(self, root):
+    def inorder(self, root, res):
         if not root:
             return None
-        self.inorder(root.left)
-        self.k -= 1
-        if self.k == 0:
-            self.ans=root.val
-            return self.ans
-        self.inorder(root.right)
-        
-        
-        #ITERATIVE -- BFS
-#         stack = []
-#         prev = float("-inf")
-#         while stack or root:
-#             while root:
-#                 stack.append(root)
-#                 root = root.left
-            
-#             root = stack.pop()
-#             if k > 0:
-#                 k -= 1
-#             if k == 0:
-#                 return root.val
-#             root = root.right
-                
-        
-        # BRUTE -- FORCE DFS
-#         res = []
-#         self.helper(root, res)
-#         return res[k-1]
-    
-#     def helper(self, root, res):
-#         if not root:
-#             return None
-#         self.helper(root.left, res)
-#         res.append(root.val)
-#         self.helper(root.right,res)
-#         return res
-        
-# in-order traversal --> left root right
-# 1 null 2 3 4 null null
+        self.inorder(root.left, res)
+        res.append(root.val)
+        self.inorder(root.right, res)
+        return res
