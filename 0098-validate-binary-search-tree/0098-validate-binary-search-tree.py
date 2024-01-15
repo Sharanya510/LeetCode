bc59424a04b5dbb4c20eb2906dc41ec253af97bd
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 13:15:14 -0800

    Time: 39 ms (89.30%), Space: 19.3 MB (30.75%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,11 +6,11 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        min_val = float("-inf")
-        max_val = float("inf")
-        return self.helper(root, min_val, max_val)
+        min_value = float("-inf")
+        max_value = float("inf")
+        return self.helper(root, min_value, max_value)
     
     def helper(self, root, min_val, max_val):
         if not root:
             return True
-        return root.val > min_val and root.val < max_val and self.helper(root.left, min_val, root.val) and self.helper(root.right, root.val, max_val)
+        return (root.val > min_val and root.val < max_val and self.helper(root.left, min_val, root.val) and self.helper(root.right, root.val , max_val))
