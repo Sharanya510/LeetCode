67eeffa4ec5424cf4fc34aab14c829d62eea91ea
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 13:00:47 -0800

    Time: 49 ms (30.33%), Space: 18.2 MB (17.26%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,20 +6,38 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return None
-        queue = deque()
-        queue.append(root)
         res = []
-        while queue:
-            temp = []
-            size = len(queue)
-            for i in range(size):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                temp.append(node.val)
-            res.append(temp)
+        if not root:
+            return res
+        self.helper(root, 0, res)
         return res
+    
+    def helper(self, node, level, res):
+        if len(res) == level:
+            res.append([])
+        res[level].append(node.val)
+        if node.left:
+            self.helper(node.left, level + 1, res)
+        if node.right:
+            self.helper(node.right, level + 1, res)
+        
+        # APPROACH --> ITERATIVE
+        # TIME COMPLEXITY --> O(N)
+        # SPACE COMPLEXITY --> O(N)
+        # if not root:
+        #     return None
+        # queue = deque()
+        # queue.append(root)
+        # res = []
+        # while queue:
+        #     temp = []
+        #     size = len(queue)
+        #     for i in range(size):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #         temp.append(node.val)
+        #     res.append(temp)
+        # return res
