a8e5b877a75dd96c6d5d46bb7550ac80c98d44ce
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-14 22:00:08 -0800

    Time: 51 ms (94.23%), Space: 25.5 MB (81.77%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,53 +8,40 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        
         first_node = head
-        mid = self.midpoint(first_node)
-        #print(mid.val)
-        second_head = self.reverse(mid.next)
-        #print(second_head.val)
-        mid.next = None
+        mid_point = self.mid(first_node)
+        second_head = self.reverse(mid_point.next)
+        mid_point.next = None
         first_head = head
-        dummy_node = ListNode(-1)
-        dummy_node.next = first_head
-
+        Dummy_Node = ListNode(-1)
+        Dummy_Node.next = first_head
+        
         while second_head:
-            next_first=first_head.next
-            next_second=second_head.next
+            next_first = first_head.next
+            next_second = second_head.next
             first_head.next = second_head
             first_head = next_first
             second_head.next = first_head
             second_head = next_second
-        return dummy_node.next          
-
-    def midpoint(self, head):
+        return Dummy_Node.next
+            
+        
+    def mid(self, head):
         slow = head
         fast = head
-        while fast.next and fast.next.next :
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         return slow
-
+    
     def reverse(self, head):
-        prev = None
         curr = head
         temp = head
-
+        prev = None
         while curr:
             curr = curr.next
             temp.next = prev
             prev = temp
             temp = curr
         return prev
-     
-        #  1 2 3 4 
-            
-#   -1     1 2->none 
-#          4 3->none
-
-# 1->4->2->3->none
-
-#          1 4  
-    
-             
+        
