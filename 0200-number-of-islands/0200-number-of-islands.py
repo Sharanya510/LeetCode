370860676ec56c932043a189afa8b22939f74cb5
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 17:19:52 -0800

    Time: 232 ms (92.12%), Space: 19.8 MB (48.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         if not grid:
             return 0
-        
-        islands = 0
-        rows, cols = len(grid), len(grid[0])
-        visit = set()
-        
-        def bfs(r,c):
-            q = collections.deque()
-            visit.add((r,c))
-            q.append((r,c))
-            while q:
-                row, col = q.popleft()
-                directions = [[1,0], [-1,0], [0,1], [0,-1]]
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if (r in range(rows) and 
-                       c in range(cols) and 
-                        grid[r][c] == "1" and
-                        (r,c) not in visit):
-                            q.append((r,c))
-                            visit.add((r,c))
-                            
-                        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visit:
-                    bfs(r,c)
-                    islands += 1
-        return islands
-                    
-        
+        m = len(grid)
+        n = len(grid[0])
+        count = 0
+        queue = deque()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    queue.append([i,j])
+                    self.bfs( queue, grid )
+        return count
+    
+    def bfs(self, queue, grid):
+        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
+        while queue:
+            x, y = queue.popleft()
+            for dirs in directions:
+                new_x = x + dirs[0]
+                new_y = y + dirs[1]
+                if new_x >=0 and new_x < len(grid) and new_y >= 0 and new_y < len(grid[0]) and grid[new_x][new_y] == "1":
+                    queue.append([new_x, new_y])
+                    grid[new_x][new_y] = "0"
