8cf807a25ef65692625f76d5aad78a5a9307f259
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-15 22:00:18 -0800

    Time: 75 ms (99.40%), Space: 19.2 MB (45.37%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,36 +1,30 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
-        if len(edges) != n-1 :
+        if len(edges) != n - 1:
             return False
         
         indegree = [0]*n
-        adj_list = defaultdict(list)
+        adjacency_list = defaultdict(list)
         queue = deque()
         
-        
         for edge in edges:
-            adj_list[edge[0]].append(edge[1])
-            adj_list[edge[1]].append(edge[0])
-            
-        for edge in edges:
+            adjacency_list[edge[0]].append(edge[1])
+            adjacency_list[edge[1]].append(edge[0])
             indegree[edge[0]] += 1
             indegree[edge[1]] += 1
             
         for i in range(len(indegree)):
             if indegree[i] == 1:
                 queue.append(i)
-       
+                
         while queue:
             parent = queue.popleft()
-            for child in adj_list[parent]:
+            for child in adjacency_list[parent]:
                 indegree[child] -= 1
                 if indegree[child] == 1:
                     queue.append(child)
-        
+                    
         for i in range(len(indegree)):
             if indegree[i] != 0:
                 return False
         return True
-            
-        
