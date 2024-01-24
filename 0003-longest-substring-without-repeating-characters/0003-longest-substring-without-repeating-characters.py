901547cabad8dfc24a503740b07ca4937bf18866
Sharanya <Sharanya.gandla05@gmail.com>
2024-01-23 22:13:31 -0800

    Time: 62 ms (50.67%), Space: 16.7 MB (59.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,34 +7,24 @@ class Solution:
         for right in range(len(s)):
             right_char = s[right]
             hash_map[right_char] += 1
-            # print(hash_map)
             if hash_map[right_char] > 1:
                 counter += 1
                 
-            while counter > 0:
-                left_char = s[left]
-                if hash_map[left_char] > 1:
-                    counter -= 1
-                hash_map[left_char] -= 1
-                left += 1
-            max_length = max(max_length , right - left + 1)
+                while counter > 0:
+                    left_char = s[left]
+                    if hash_map[left_char] > 1:
+                        counter -= 1
+                    hash_map[left_char] -= 1
+                    left += 1
+            max_length = max(max_length, right - left + 1)
         return max_length
-            
-            
-        # APPROACH --> BRUTE FORCE
-        # TIME COMPLEXITY --> O(N3)
-        # SPACE COMPLEXITY --> O(N)
-#         longest = 0
-#         for i in range(len(s)):
-#             for j in range(i, len(s)):
-#                 if self.check(i, j, s):
-#                     longest = max(longest, j - i +1)
-#         return longest
+                        
+                        
+# a   b   c   a   b   c   b   b
+# r   
+# l
+#     r
+#         r
+#             r
     
-#     def check(self, start, end, s):
-#         chars = set()
-#         for i in range(start, end + 1):
-#             if s[i] in chars:
-#                 return False
-#             chars.add(s[i])
-#         return True
+                
