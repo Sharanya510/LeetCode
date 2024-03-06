8de51875b196634c554557e2a377800fc13ff8ec
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-05 19:39:28 -0800

    Time: 59 ms (31.96%), Space: 16.5 MB (88.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -11,10 +11,12 @@ class Solution:
         if target < 0 or i == len(candidates):
             return
         if target == 0:
+            # with this line it is performing shallow copy, which means whatever is in the path, it gets reflected in the res and it is giving the empty array as output, this doesnt work
+            # res.append(path)
+            # inorder to avoid this, we need to do deep copy which means need to create an independent array that can store the combinations that we get from the path list into the res list, so the below code works
             res.append(path[:])
             return
        
-        
       # without choosing element
         self.helper(candidates, target, i+1, path, res)
         path.append(candidates[i])
