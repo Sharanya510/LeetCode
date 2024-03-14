9f56c4c34dfd764ff2c7dff854a14ecca0b23017
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-13 21:17:24 -0700

    Time: 83 ms (91.13%), Space: 18.5 MB (33.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -31,39 +31,5 @@ class Solution:
         for i in range(len(indegree)):
             if indegree[i] != 0:
                 return []
-        
         return res
         
-        
-#         res = []
-#         if not prerequisites:
-#             for i in range(numCourses-1, -1, -1):
-#                 res.append(i)
-#             return res
-        
-#         indegree = [0]*numCourses
-#         adjacency_list = defaultdict(list)
-#         # res = []
-        
-#         for prereq in prerequisites:
-#             indegree[prereq[0]] += 1
-#             adjacency_list[prereq[1]].append(prereq[0])
-            
-#         queue = deque()
-#         for i in range(len(indegree)):
-#             if indegree[i] == 0:
-#                 queue.append(i)
-#                 res.append(i)
-                
-#         while queue:
-#             new_value = queue.popleft()
-#             for child in adjacency_list[new_value]:
-#                 indegree[child] -= 1
-#                 if indegree[child] == 0:
-#                     queue.append(child)
-#                     res.append(child)
-                    
-#         if i in range(len(indegree)):
-#             if indegree[i] != 0:
-#                 return []
-#         return res
