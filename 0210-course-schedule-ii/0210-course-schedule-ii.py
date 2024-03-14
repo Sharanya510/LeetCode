31434c67033529667d7721b6f51a03b6e422defa
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-13 21:15:31 -0700

    Time: 96 ms (30.57%), Space: 18.5 MB (50.83%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,69 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        if not prerequisites:
+            for i in range(numCourses-1, -1, -1):
+                res.append(i)
+            return res
+        
+        indegree = [0]*numCourses
+        adjacency_list = defaultdict(list)
+        
+        for prereq in prerequisites:
+            indegree[prereq[0]] += 1
+            adjacency_list[prereq[1]].append(prereq[0])
+            
+        queue = deque()
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+                res.append(i)
+                
+        while queue:
+            new_value = queue.popleft()
+            for child in adjacency_list[new_value]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    queue.append(child)
+                    res.append(child)
+        
+        # Check if there's any remaining course with indegree not equal to 0
+        for i in range(len(indegree)):
+            if indegree[i] != 0:
+                return []
+        
+        return res
+        
+        
+#         res = []
+#         if not prerequisites:
+#             for i in range(numCourses-1, -1, -1):
+#                 res.append(i)
+#             return res
+        
+#         indegree = [0]*numCourses
+#         adjacency_list = defaultdict(list)
+#         # res = []
+        
+#         for prereq in prerequisites:
+#             indegree[prereq[0]] += 1
+#             adjacency_list[prereq[1]].append(prereq[0])
+            
+#         queue = deque()
+#         for i in range(len(indegree)):
+#             if indegree[i] == 0:
+#                 queue.append(i)
+#                 res.append(i)
+                
+#         while queue:
+#             new_value = queue.popleft()
+#             for child in adjacency_list[new_value]:
+#                 indegree[child] -= 1
+#                 if indegree[child] == 0:
+#                     queue.append(child)
+#                     res.append(child)
+                    
+#         if i in range(len(indegree)):
+#             if indegree[i] != 0:
+#                 return []
+#         return res
