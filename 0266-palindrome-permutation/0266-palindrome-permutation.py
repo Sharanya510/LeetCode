f679f59a8df81ac37335e881e80965b86affb376
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-13 20:51:01 -0700

    Time: 34 ms (60.70%), Space: 16.7 MB (25.02%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.py
+++ 0266-palindrome-permutation/0266-palindrome-permutation.py
@@ -4,13 +4,12 @@ class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
         my_hashmap = Counter(s)
         
-        count_odd, count_even = 0, 0
-        print(my_hashmap)
+        count_odd = 0
+        
         for key, value in my_hashmap.items():
             if value % 2 == 1:
                 count_odd += 1
-            else:
-                count_even += 1
+                
         if count_odd > 1 :
             return False
         else:
