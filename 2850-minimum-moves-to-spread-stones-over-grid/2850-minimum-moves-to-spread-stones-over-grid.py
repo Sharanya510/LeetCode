d6003156a16308f9730f82a63f2dce1da5c9daca
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-16 16:58:30 -0700

    Time: 51 ms (94.00%), Space: 16.7 MB (46.22%) - LeetHub

---
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumMoves(self, grid: List[List[int]]) -> int:
+        extras, zeroes = defaultdict(int), []
+
+        for row in range(3):
+            for col in range(3):
+                if grid[row][col] > 1:
+                    extras[(row, col)] = grid[row][col]
+                if grid[row][col] == 0:
+                    zeroes.append((row, col))
+        return self.backtrack(0, zeroes, extras)
+    
+    def backtrack(self, index, zeroes, extras):
+        if index == len(zeroes):
+            return 0
+        dist = inf
+        curr_zero_x, curr_zero_y = zeroes[index]
+            
+        for key, val in extras.items():
+            if val > 1:
+                extras[key] -= 1
+                dist = min(dist, self.backtrack(index + 1, zeroes, extras) + abs(curr_zero_x - key[0])+ abs(curr_zero_y - key[1]))
+                extras[key] += 1
+        return dist
