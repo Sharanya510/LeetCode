9a0ed73dbc1d2cb838e629de743f70e68b794a14
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-17 23:54:14 -0700

    Time: 67 ms (82.87%), Space: 17.1 MB (53.56%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -11,9 +11,6 @@ class Solution:
             curr = nums[i]
             temp_max = max(curr, max_so_far * curr, min_so_far * curr)
             min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
-
             max_so_far = temp_max
-
             result = max(max_so_far, result)
-
         return result
