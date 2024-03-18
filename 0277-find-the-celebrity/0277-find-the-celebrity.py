4a88dfb90c80a20875824c6a23083ea98c508c4c
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-17 19:02:12 -0700

    Time: 709 ms (86.48%), Space: 16.7 MB (86.48%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.py
+++ 0277-find-the-celebrity/0277-find-the-celebrity.py
@@ -4,17 +4,32 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
+        celeb = 0
+
+        for people in range(n):
+            if knows(celeb, people):
+                celeb = people
+
+        for people in range(n):
+            if people == celeb:
+                continue;
+
+            if (knows(celeb,people) or not knows(people, celeb)):
+                return -1
+
+        return celeb
+        
         # APPROACH --> BRUTE FORCE
-        numberofpeople = n
-        for i in range(n):
-            if self.isCelebrity(i, numberofpeople):
-                return i
-        return -1
+#         numberofpeople = n
+#         for i in range(n):
+#             if self.isCelebrity(i, numberofpeople):
+#                 return i
+#         return -1
     
-    def isCelebrity(self, i, numberofpeople):
-        for j in range(numberofpeople):
-            if i == j:
-                continue
-            if knows(i , j ) or not knows(j, i) :
-                return False
-        return True
+#     def isCelebrity(self, i, numberofpeople):
+#         for j in range(numberofpeople):
+#             if i == j:
+#                 continue
+#             if knows(i , j ) or not knows(j, i) :
+#                 return False
+#         return True
