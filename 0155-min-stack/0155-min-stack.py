49f0b425d033bb954dacd3d12b377d8898a488c9
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-20 15:48:17 -0700

    Time: 56 ms (52.05%), Space: 20.3 MB (78.80%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -3,29 +3,42 @@ class MinStack:
     def __init__(self):
         self.main_stack = []
         self.min_stack = []
+        self.minimum = float("inf")
         
     def push(self, val: int) -> None:
         self.main_stack.append(val)
-        if not self.min_stack or val <= self.min_stack[-1]:
+        if not self.min_stack or val <= self.minimum:
+            self.minimum = val
             self.min_stack.append(val)
 
     def pop(self) -> None:
-        if self.min_stack[-1]==self.main_stack[-1]:
-            self.min_stack.pop()
-        
-        self.main_stack.pop()
+        if self.main_stack:
+            value = self.main_stack[-1]
+            if self.min_stack and value == self.min_stack[-1]:
+                self.main_stack.pop()
+                self.min_stack.pop()
+            elif self.main_stack:
+                self.main_stack.pop()
+        if len(self.min_stack) == 0:
+            self.minimum = float("inf")
+        else:
+            self.minimum = self.min_stack[-1]
 
     def top(self) -> int:
-        return self.main_stack[-1]
+        if self.main_stack:
+            return self.main_stack[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        if self.min_stack:
+            return self.min_stack[-1]
 
 # ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
 # [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
 
-# main --> [2147483646  ]
-# min --> [2147483646 ]
+# main --> [ 2147483646, 2147483646, 2147483647 ]
+# min --> [ 2147483646,  ]
+        
+        
         
         
 # Your MinStack object will be instantiated and called as such:
