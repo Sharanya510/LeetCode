8751731fe3ede35a8a5faa941d64ff49b92f90c4
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-21 11:26:55 -0700

    Time: 35 ms (98.55%), Space: 17 MB (12.20%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,14 +1,33 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = [0]*len(nums1)
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    number = nums2[j]
-                    for k in range(j, len(nums2)):
-                        if nums2[k] > number:
-                            ans[i] = nums2[k]
-                            break
-                        else:
-                            ans[i] = -1
-        return ans
+        my_stack = []
+        my_hashmap = {}
+        
+        for num in nums2:
+            while my_stack and num > my_stack[-1]:
+                my_hashmap[my_stack.pop()] = num
+            my_stack.append(num)
+        
+        while my_stack:
+            my_hashmap[my_stack.pop()] = -1
+            
+        res = []
+        for num in nums1:
+            res.append(my_hashmap.get(num, -1))
+        return res
+            
+        
+        
+        
+        # ans = [0]*len(nums1)
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j]:
+        #             number = nums2[j]
+        #             for k in range(j, len(nums2)):
+        #                 if nums2[k] > number:
+        #                     ans[i] = nums2[k]
+        #                     break
+        #                 else:
+        #                     ans[i] = -1
+        # return ans
