2019711b40e8d762e8d2eaca4c70ae43b1de335c
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-25 00:53:04 -0700

    Time: 190 ms (55.03%), Space: 19.2 MB (84.88%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        ROWS = len(rooms)
+        COLS = len(rooms[0])
+        stack = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    stack.append((r,c))
+        level = 1
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        nextStack = []
+        while stack:
+            r, c = stack.pop()
+            for x, y in directions:
+                if r+x >= 0 and c+y >= 0 and r+x<ROWS and c+y<COLS and rooms[r+x][c+y] == 2147483647:
+                    nextStack.append((r+x,c+y))
+                    rooms[r+x][c+y] = level
+            if stack == [] and nextStack != []:
+                stack = nextStack
+                nextStack = []
+                level += 1
