c8cec2614709bbe1a337973e373fb831e921f5ec
Sharanya <Sharanya.gandla05@gmail.com>
2024-03-31 16:04:32 -0700

    Time: 31 ms (88.34%), Space: 16.6 MB (79.74%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.py
+++ 0666-path-sum-iv/0666-path-sum-iv.py
@@ -3,16 +3,12 @@ class Solution:
         dic ={}
         for num in nums: 
             depth= num//100
-            position = num%100//10
-            value = num%100%10
+            position = num// 10 % 10
+            value = num % 10
             dic[(depth,position)] = value
         root = (nums[0]//100,nums[0]%100//10)
         stack =[(root,0)]
         ans = 0
-       # the postion of left node for a node should be (row+1,2*col-1) and 
-       # the right node should be (row+1,2*col) since the descpriton explains that
-       # its position should be as same as in the full binary tree
-       #conduct dfs search 
         while stack:
             node,curr = stack.pop()
             depth,position = node
