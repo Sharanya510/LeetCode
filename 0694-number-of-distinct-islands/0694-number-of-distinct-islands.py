d977a18ee54a665918d60d9ba3e6e2f84b1f7cbe
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-01 16:44:59 -0700

    Time: 168 ms (41.86%), Space: 18.6 MB (23.19%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -1,17 +1,5 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        
-        def current_island_is_unique():
-            for other_island in unique_islands:
-                if len(other_island) != len(current_island):
-                    continue
-                for cell_1, cell_2 in zip(current_island, other_island):
-                    if cell_1 != cell_2:
-                        break
-                else:
-                    return False
-            return True
-            
         # Do a DFS to find all cells in the current island.
         def dfs(row, col):
             if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
@@ -19,7 +7,7 @@ class Solution:
             if (row, col) in seen or not grid[row][col]:
                 return
             seen.add((row, col))
-            current_island.append((row - row_origin, col - col_origin))
+            current_island.add((row - row_origin, col - col_origin))
             dfs(row + 1, col)
             dfs(row - 1, col)
             dfs(row, col + 1)
@@ -27,15 +15,13 @@ class Solution:
         
         # Repeatedly start DFS's as long as there are islands remaining.
         seen = set()
-        unique_islands = []
+        unique_islands = set()
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                current_island = []
+                current_island = set()
                 row_origin = row
                 col_origin = col
                 dfs(row, col)
-                if not current_island or not current_island_is_unique():
-                    continue
-                unique_islands.append(current_island)
-        # print(unique_islands)
+                if current_island:
+                    unique_islands.add(frozenset(current_island))
         return len(unique_islands)
