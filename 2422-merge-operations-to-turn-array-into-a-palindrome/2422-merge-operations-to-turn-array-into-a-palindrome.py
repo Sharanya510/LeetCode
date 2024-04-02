c28d6276c97e2c187c065ee19ba9c2ae0fe56293
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-02 09:00:47 -0700

    Time: 645 ms (86.57%), Space: 30.7 MB (83.90%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -1,21 +1,39 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        low, high, count = 0, n-1, 0
-        while low <= high:
-            if nums[low] == nums[high]:
-                low += 1 
-                high -= 1 
-            elif nums[low] < nums[high]:
-                count += 1
-                nums[low+1] += nums[low]
-                low += 1
-            else:
-                count += 1
-                nums[high-1] += nums[high]
-                high -= 1
+        left, right = 0, len(nums) - 1
+        left_sum, right_sum = 0, 0
+        ans = 0
+        while left < right:
+            left_sum += nums[left]
+            right_sum += nums[right]
+            while left < right and left_sum != right_sum:
+                if left_sum > right_sum:
+                    right -= 1
+                    right_sum += nums[right]
+                    ans += 1
+                else:
+                    left += 1
+                    left_sum += nums[left]
+                    ans += 1
+            left += 1
+            right -= 1
+        return ans
+#         n = len(nums)
+#         low, high, count = 0, n-1, 0
+#         while low <= high:
+#             if nums[low] == nums[high]:
+#                 low += 1 
+#                 high -= 1 
+#             elif nums[low] < nums[high]:
+#                 count += 1
+#                 nums[low+1] += nums[low]
+#                 low += 1
+#             else:
+#                 count += 1
+#                 nums[high-1] += nums[high]
+#                 high -= 1
 
-        return count
+#         return count
         
         # left, right = 0, len(nums)-1
         # count = 0
