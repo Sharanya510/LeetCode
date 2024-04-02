558b135ec562a197d722bbaf4eb5f554d60be7e7
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-02 13:58:06 -0700

    Time: 80 ms (60.72%), Space: 21.2 MB (28.11%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,34 +1,11 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        stck = []    
-        
-        for c in s:                            
-            if stck and stck[-1][0] == c: # check if stack is not empty
-                stck[-1][1]+=1
-                if stck[-1][1] == k:
-                    stck.pop()
+        my_stack = []
+        for c in s:
+            if len(my_stack) != 0 and my_stack[-1][0] == c:
+                my_stack[-1][1] += 1
+                if my_stack[-1][1] == k:
+                    my_stack.pop()
             else:
-                stck.append([c, 1])            
-        
-        return ''.join(c * cnt for c, cnt in stck)
-        
-#         left = 0
-#         right = left + 1
-#         while right < len(s)-1:
-#             if len(s) <= k:
-#                 return s
-            
-#             if s[left] != s[right]:
-#                 left += 1
-#                 right = left+1
-#             else:
-#                 while (right - left + 1) != k:
-#                     right += 1
-                
-#                 if (right - left + 1) == k and s[left] == s[right] :
-#                     s = s[:left] + s[right+1:]
-#                     left, right = 0, 1
-#         return s
-
-        # len(s[left:right+1])
-    
+                my_stack.append([c, 1])
+        return ''.join( c*count for c, count in my_stack)
