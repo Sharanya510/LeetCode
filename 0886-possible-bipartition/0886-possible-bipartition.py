6b47c16c74d0da96bcfc31fe1e5e63691982cec2
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-01 18:15:03 -0700

    Time: 575 ms (56.18%), Space: 21.9 MB (47.12%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,10 +1,6 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        indegree = [0]*n
         adj_list = defaultdict(list)
-        group_A = set()
-        group_B = set()
-        
         for dislike in dislikes:
             adj_list[dislike[0]].append(dislike[1])
             adj_list[dislike[1]].append(dislike[0])
@@ -20,10 +16,8 @@ class Solution:
         queue = deque()
         queue.append(source)
         color[source] = 0
-        
         while queue:
             node = queue.popleft()
-        
             for neighbor in adj_list[node]:
                 if color[neighbor] == color[node]:
                     return False
