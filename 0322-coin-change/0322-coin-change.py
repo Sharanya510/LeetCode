fa072183ce2bdb671c0355253f4d9d388f2c4061
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-03 12:34:19 -0700

    Time: 1199 ms (12.63%), Space: 45.4 MB (5.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,31 +1,52 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #using dynamic programming
-        dp = [amount+1] * (amount+1)
-        dp[0] = 0
         
-        for a in range(amount+1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1+dp[a-c])
+        mem = {}
+        def dp(amt):
+            if amt == 0:
+                return 0
+            if amt in mem:
+                return mem[amt]
+            res = float('inf')
+            for coin in coins:
+                if amt - coin >= 0:
+                    res = min(res, 1 + dp(amt -coin))
+            mem[amt] = res
+            return res
         
-        return dp[amount] if dp[amount]!= amount + 1 else -1
+        res = dp(amount)
+        if res == float('inf'):
+            return -1
+        else:
+            return res
         
-        # brute-force
-        # numberofcoins, num = -1,0
-        # total = 0
-        # num1, num2 = 0, 0
-        # for n in coins:
-        #     while total <= amount:
-        #         temp = max(n+num1, n+num2)
-        #         num1 = num2
-        #         num2 = temp
-        #         total += num1 + num2
-        #         numberofcoins += 1
-        #     num = numberofcoins
-        #     numberofcoins = min(numberofcoins, num )
-        # return numberofcoins
-                
-                
+        
+#         BRUTE FORCE -- LINE 5 TO 19
+#         def dp(amt):
+#             if amt == 0:
+#                 return 0
             
+#             res = float('inf')
+#             for coin in coins:
+#                 if amt - coin >= 0:
+#                     res = min(res, 1 + dp(amt -coin))
+            
+#             return res
+#         res = dp(amount)
+#         if res == float('inf'):
+#             return -1
+#         else:
+#             return res
         
+        
+        
+#         dp = [float('inf')]*(amount+1)
+#         dp[0] = 0
+#         for amt in range(1, amount+1):
+#             for coin in coins:
+#                 if  amt - coin >= 0:
+#                     dp[i] = min(dp[i], 1 + dp[amt-coin])
+#         return dp[-1]
+        
+        
+             
