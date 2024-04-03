7d30b620b244c7054248412214ea955415b1cff9
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-03 13:50:08 -0700

    Time: 35 ms (63.09%), Space: 16.5 MB (44.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0,0
-        #[rob1, rob2, n, n+1]
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
-            
-        
+        if not nums:
+            return 0
+        maxRobbedAmount = [None]*(len(nums) + 1)
+        N = len(nums)
+        # Base case initialization.
+        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
+        # DP table calculations.
+        for i in range(N - 2, -1, -1):
+            # Same as recursive solution.
+            maxRobbedAmount[i] = max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i])
+        return maxRobbedAmount[0]  
+    
+        # even_sum, odd_sum = 0, 0
+        # for i in range(0, len(nums), 2):
+        #     even_sum += nums[i]
+        # for j in range(1, len(nums), 2):
+        #     odd_sum += nums[j]
+        # res = max(even_sum, odd_sum)
+        # return res
