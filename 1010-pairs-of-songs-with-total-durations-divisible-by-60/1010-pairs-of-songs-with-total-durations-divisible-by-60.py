5928e36e4a8f31dc09ae67b68bd9b3205472fc91
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-03 23:24:20 -0700

    Time: 229 ms (53.32%), Space: 20.8 MB (57.42%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        remainders = collections.defaultdict(int)
+        ret = 0
+        for t in time:
+            if t % 60 == 0: # check if a%60==0 && b%60==0
+                ret += remainders[0]
+            else: # check if a%60+b%60==60
+                ret += remainders[60-t%60]
+            remainders[t % 60] += 1 # remember to update the remainders
+        return ret
+        
+                
+            
+        
+        
+        # BRUTE FORCE -- TLE
+        # count = 0
+        # for i in range(len(time)):
+        #     for j in range(i, len(time)):
+        #         if i < j and (time[i] + time[j]) % 60 == 0:
+        #             # print(time[i], time[j])
+        #             count +=1
+        # return count
