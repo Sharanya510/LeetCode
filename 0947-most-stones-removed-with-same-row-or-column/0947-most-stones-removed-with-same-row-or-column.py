d23250776e1e744388521efd6f697268d8ed6950
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-07 11:01:59 -0700

    Time: 998 ms (6.66%), Space: 17.2 MB (91.23%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,37 +1,24 @@
 class Solution:
     def shareSameRowOrColumn(self, a, b):
-        # Return true if stone a and b shares row or column
         return a[0] == b[0] or a[1] == b[1]
 
     def dfs(self, stones, adj, visited, src):
-        # Mark the stone as visited
-        visited[src] = 1
-        
-        # Iterate over the adjacent, and recurse over it if not visited yet
+        visited.add(src)
         for adjacent in adj[src]:
-            if visited[adjacent] == 0:
+            if adjacent not in visited:
                 self.dfs(stones, adj, visited, adjacent)
 
     def removeStones(self, stones):
-        # Adjacency list to store edges
         adj = defaultdict(list)
-        
         for i in range(len(stones)):
             for j in range(i + 1, len(stones)):
                 if self.shareSameRowOrColumn(stones[i], stones[j]):
                     adj[i].append(j)
                     adj[j].append(i)
-        
-        # Array to mark visited stones
-        visited = [0] * len(stones)
-        # Counter for connected components
+        visited = set()
         componentCount = 0
         for i in range(len(stones)):
-            if visited[i] == 0:
-                # If the stone is not visited yet,
-                # Start the DFS and increment the counter
+            if i not in visited:
                 componentCount += 1
                 self.dfs(stones, adj, visited, i)
-        
-        # Return the maximum stones that can be removed
         return len(stones) - componentCount
