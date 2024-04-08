77bebaf9f0239be10edf934616c161d473ddb21a
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-07 21:46:34 -0700

    Time: 542 ms (12.97%), Space: 26.7 MB (84.73%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.py
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.py
@@ -10,9 +10,12 @@ class Solution:
         for q in queries:
             num_reverse = ''
             n_num = num + q-1
+            # print(n_num)
             for i in range(len(str(n_num))-1-even, -1, -1):
                 num_reverse += (str(n_num)[i])
+                # print(num_reverse)
             pal = str(n_num) +  num_reverse
+            # print(pal)
             if len(pal) > intLength:
                 output.append(-1) 
             else:
