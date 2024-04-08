4053e8a884c9d1e91947f15304a499cd7d9b5558
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-08 00:45:04 -0700

    Time: 261 ms (73.88%), Space: 18.1 MB (57.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -12,16 +12,4 @@ class Solution:
             else:
                 left = middle + 1
         return right
-    
-    
-        # approach --> bruteforce
-        # speed = 1
-        # while True:
-        #     hour_spent = 0
-        #     for pile in piles:
-        #         hour_spent += math.ceil(pile / speed)    
-        #     if hour_spent <= h:
-        #         return speed
-        #     else:
-        #         speed += 1
         
