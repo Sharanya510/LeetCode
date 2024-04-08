f27f735006fe2a75b55590f5c193691efc10a3f9
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-08 00:01:54 -0700

    Time: 66 ms (28.16%), Space: 16.6 MB (60.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -9,15 +9,12 @@ class Solution:
             hash_map[right_char] += 1
             if hash_map[right_char] > 1:
                 counter += 1
-                
                 while counter > 0:
                     left_char = s[left]
                     if hash_map[left_char] > 1:
                         counter -= 1
                     hash_map[left_char] -= 1
                     left += 1
-                    
-                    
             max_length = max(max_length, right - left + 1)
         return max_length
                 
