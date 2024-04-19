be59bb0009437961957b2e03a63541d181a020c7
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-18 20:27:45 -0700

    Time: 56 ms (59.29%), Space: 16.7 MB (9.69%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -7,14 +7,12 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         s1 = []
         s2 = []
-
         while l1:
             s1.append(l1.val)
             l1 = l1.next
         while l2:
             s2.append(l2.val)
             l2 = l2.next
-
         total_sum = 0
         carry = 0
         ans = ListNode()
@@ -23,13 +21,10 @@ class Solution:
                 total_sum += s1.pop()
             if s2:
                 total_sum += s2.pop()
-
             ans.val = total_sum % 10
             carry = total_sum // 10
             head = ListNode(carry)
             head.next = ans
             ans = head
             total_sum = carry
-
         return ans.next if carry == 0 else ans
-        
