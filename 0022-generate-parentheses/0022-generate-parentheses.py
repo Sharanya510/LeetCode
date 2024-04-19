c05d0c4924a27f6deab9e18e5b69b03392b462e8
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-18 21:51:42 -0700

    Time: 32 ms (87.07%), Space: 16.8 MB (42.44%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -8,13 +8,14 @@ class Solution:
     
     def backtrack(self,res, curr_string, opencount, closedcount, n):
         if len(curr_string) == 2 * n:
+            # print("result is: ", res)
             res.append(curr_string)
             return 
-        
+        # print("first loop: ", curr_string)
         if (opencount < n):
             self.backtrack(res, curr_string + "(", opencount + 1, closedcount, n)
-            
+        # print("second loop: ", curr_string)
         if (opencount > closedcount):
             self.backtrack(res, curr_string + ")", opencount, closedcount+1, n)
-        
+        # print("third loop: ", curr_string)
         
