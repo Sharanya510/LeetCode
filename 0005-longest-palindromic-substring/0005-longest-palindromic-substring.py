bb020df73c882a2c39976cbe200fc2ecccff4470
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-18 21:16:28 -0700

    Time: 2954 ms (26.10%), Space: 16.5 MB (97.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        resLen = 0
-        
-        for i in range(len(s)):
-            #odd length
-            l,r = i,i
-            while l >= 0 and r < len(s) and s[l]==s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l -= 1
-                r += 1
+        def check(i, j):
+            left = i
+            right = j - 1
+            
+            while left < right:
+                if s[left] != s[right]:
+                    return False
                 
-            #even length
-            l,r = i,i+1
-            while l >= 0 and r < len(s) and s[l]==s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                l -= 1
-                r += 1
-        return res
+                left += 1
+                right -= 1
+            
+            return True
+        
+        for length in range(len(s), 0, -1):
+            for start in range(len(s) - length + 1):
+                if check(start, start + length):
+                    return s[start:start + length]
+
+        return ""
+                
+# b   a   b   a   d
+# l
+# r
+#     r
+#         r
+#             r
+        
+        
+        # longest = 0
+        # left, right = 0, 0
+        # while left <= right:
+        #     substring = s[left: right+1]
+        #     reverse_substring = substring[::-1]
+        #     if substring == reverse_substring:
+        #         longest = max(longest, len(substring))
+        #     right += 1
+        # return longest
         
