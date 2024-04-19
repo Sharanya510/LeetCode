364c3ab95120bef24aa0755577e10aa4d78961f6
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-18 21:26:26 -0700

    Time: 50 ms (19.64%), Space: 17.2 MB (52.37%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -15,3 +15,13 @@ class Solution:
                     max_length = max(max_length, i - stack[-1])  
                 
         return max_length
+    
+#     )   (   )   (   )   )
+# [-1]
+# []
+#     [0]
+#         [0, 1]
+#             [0] maxlen = 2
+#                  [0, 3]
+#                     [0] max_len = 4
+#                          [5]
