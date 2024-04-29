396f08251d2b2681bc4ac0a65501db88c7541052
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-29 15:45:08 -0700

    Time: 28 ms (92.22%), Space: 16.6 MB (22.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,10 +8,10 @@ class Solution:
             if c in stack_map:
                 my_stack.append(stack_map[c])
             elif my_stack:
-                    if my_stack[-1] == c:
-                        my_stack.pop()
-                    else:
-                        return False
+                if my_stack[-1] == c:
+                    my_stack.pop()
+                else:
+                    return False
             else:
                 return False
         return len(my_stack) == 0
