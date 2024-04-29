7b3ae7faaadb20c4823ac0117f28a13719270b80
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-29 13:21:12 -0700

    Time: 121 ms (13.53%), Space: 18.6 MB (39.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,42 +1,38 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # Case of empty height list
+        if len(height) == 0:
+            return 0
         ans = 0
-        current = 0
-        st = []
-        while current < len(height):
-            # If current height is greater than the height of the bar at stack top,
-            # it means we can potentially trap water.
-            while st and height[current] > height[st[-1]]:
-                top = st.pop()
-                if not st:
-                    break
-                # Calculate the distance and the bounded height.
-                distance = current - st[-1] - 1
-                bounded_height = min(height[current], height[st[-1]]) - height[top]
-                ans += distance * bounded_height
-            st.append(current)
-            current += 1
-            # print(st)
+        size = len(height)
+        # Create left and right max arrays
+        left_max, right_max = [0] * size, [0] * size
+        # Initialize first height into left max
+        left_max[0] = height[0]
+        for i in range(1, size):
+            # update left max with current max
+            left_max[i] = max(height[i], left_max[i - 1])
+        # Initialize last height into right max
+        right_max[size - 1] = height[size - 1]
+        for i in range(size - 2, -1, -1):
+            # update right max with current max
+            right_max[i] = max(height[i], right_max[i + 1])
+        # Calculate the trapped water
+        for i in range(1, size - 1):
+            ans += min(left_max[i], right_max[i]) - height[i]
+        # Return amount of trapped water
         return ans
         
         
-        
-        
-#         ans = 0
-#         size = len(height)
-#         for i in range(1, size - 1):
-#             left_max = 0
-#             right_max = 0
-            
-#             # Search the left part for max bar size
-#             for j in range(i, -1, -1):
-#                 left_max = max(left_max, height[j])
-            
-#             # Search the right part for max bar size
-#             for j in range(i, size):
-#                 right_max = max(right_max, height[j])
-            
-#             # Calculate the trapped water at current bar
-#             ans += min(left_max, right_max) - height[i]
-#             # print(ans)
-#         return ans
+        # APPROACH -- BRUTE FORCE
+        # ans = 0
+        # size = len(height)
+        # for i in range(1, size - 1):
+        #     left_max = 0
+        #     right_max = 0
+        #     for j in range(i, -1, -1):  # Search the left part for max bar size
+        #         left_max = max(left_max, height[j])
+        #     for j in range(i, size):  # Search the right part for max bar size
+        #         right_max = max(right_max, height[j])
+        #     ans += min(left_max, right_max) - height[i]
+        # return ans
