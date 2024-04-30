10c303b5740983ef9a2eb2fa10d66151826043f5
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-30 10:48:30 -0700

    Time: 21 ms (63.66%), Space: 11.9 MB (60.24%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,27 +1,18 @@
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        # APPROACH --> BINARY SEARCH
-        # TIME COMPLEXITY --> O(LOG N)
-        # SPACE COMPLEXITY --> O(1)
-        low, high = 0, len(nums)-1
-        if nums[low] < nums[high]:
-            return nums[low]
-        if len(nums) == 1:
+class Solution(object):
+    def findMin(self, nums):
+        if len(nums)==1:
             return nums[0]
-        while low <= high:
-            mid = (low + high)//2
-            if nums[mid] > nums[mid+1]:
+        low=0
+        high=len(nums)-1
+        if nums[low]<nums[high]:
+            return nums[low]
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>nums[mid+1]:
                 return nums[mid+1]
-            if nums[mid - 1] > nums[mid]:
+            if nums[mid]<nums[mid-1] and nums[mid]<nums[mid+1]:
                 return nums[mid]
-            if nums[mid] < nums[high] and nums[mid] < nums[low]:
-                high = mid - 1
+            if nums[low]<nums[mid]:
+                low=mid+1
             else:
-                low = mid +1
-            
-            
-            
-            
-            
-
-        
+                high=mid-1
