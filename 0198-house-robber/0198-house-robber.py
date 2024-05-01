18acb8b9f0cde962519371057f9a8171a3ac63cb
Sharanya <Sharanya.gandla05@gmail.com>
2024-05-01 15:24:20 -0700

    Time: 32 ms (79.65%), Space: 16.4 MB (99.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,29 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-    
     def rob(self, nums: List[int]) -> int:
-        self.memo = {}
-        return self.robFrom(0, nums)
-    
-    def robFrom(self, i, nums):
-        if i >= len(nums):
+        if not nums:
             return 0
-        if i in self.memo:
-            return self.memo[i]
-        ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
-        self.memo[i] = ans
-        return ans
+        maxRobbedAmount = [None for _ in range(len(nums) + 1)]
+        N = len(nums)
+        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
+        for i in range(N - 2, -1, -1):
+            maxRobbedAmount[i] = max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i])
+            
+        return maxRobbedAmount[0]    
+    
+    
+    #APPROACH -- TOP DOWN
+#     def __init__(self):
+#         self.memo = {}
+    
+#     def rob(self, nums: List[int]) -> int:
+#         self.memo = {}
+#         return self.robFrom(0, nums)
+    
+#     def robFrom(self, i, nums):
+#         if i >= len(nums):
+#             return 0
+#         if i in self.memo:
+#             return self.memo[i]
+#         ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
+#         self.memo[i] = ans
+#         return ans
