b58d3fabddd3872d7997419f1b8ca8592d3f59fb
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-30 19:17:21 -0700

    Time: 179 ms (72.16%), Space: 19.4 MB (73.56%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -7,6 +7,7 @@ class Solution:
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     stack.append((r,c))
+        # print(stack)
         level = 1
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
         nextStack = []
@@ -16,6 +17,7 @@ class Solution:
                 if r+x >= 0 and c+y >= 0 and r+x<ROWS and c+y<COLS and rooms[r+x][c+y] == 2147483647:
                     nextStack.append((r+x,c+y))
                     rooms[r+x][c+y] = level
+            # print(rooms)
             if len(stack) == 0 and len(nextStack) != 0:
                 stack = nextStack
                 nextStack = []
