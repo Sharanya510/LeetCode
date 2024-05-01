627311164eadc1da93c6148fcfecd9f20adcc86f
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-30 22:23:58 -0700

    Time: 54 ms (68.55%), Space: 17.7 MB (53.81%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
@@ -1,24 +1,17 @@
 class Solution:
     def minimumKeypresses(self, s: str) -> int:
-        # APPROACH --> BRUTE FORCE
-        # TIME COMPLEXITY --> O(N log N)
-        # SPACE COMPLEXITY --> O(N)
-        freq = {}
-        for c in s:
-            if c not in freq:
-                freq[c] = 1
-            else:
-                freq[c] += 1
-        # print(freq)
-        freq_array = [[key, freq[key]] for key in freq.keys()]
-        # print(freq_array)
-        freq_array.sort(key=lambda x:x[1], reverse=True)
-        # print(freq_array)
-        total_press = 0
-        single_press = 1
-        for index, char in enumerate(freq_array):
-            total_press += char[1]*single_press
-            if (index+1)%9 == 0:
-                single_press += 1
+        hash_map=Counter(s)
+        freq_array=[[key,hash_map[key]] for key in hash_map.keys()]
+        freq_array = sorted(freq_array,key=lambda x:x[1], reverse=True)
+        total_press=0
+        single_press=1
+        for index,char in enumerate(freq_array):
+            total_press+=char[1]*single_press
+            if (index+1)%9==0:
+                single_press+=1
         return total_press
+    
             
+        
+            
+        
