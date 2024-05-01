e471bddde6dd3f3b6f4ae6849ebcac9e02a11de1
Sharanya <Sharanya.gandla05@gmail.com>
2024-05-01 15:22:51 -0700

    Time: 35 ms (62.61%), Space: 16.4 MB (89.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,16 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        maxRobbedAmount = [None]*(len(nums) + 1)
-        N = len(nums)
-        # Base case initialization.
-        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
-        # DP table calculations.
-        for i in range(N - 2, -1, -1):
-            # Same as recursive solution.
-            maxRobbedAmount[i] = max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i])
-        return maxRobbedAmount[0]  
+    def __init__(self):
+        self.memo = {}
     
-        # even_sum, odd_sum = 0, 0
-        # for i in range(0, len(nums), 2):
-        #     even_sum += nums[i]
-        # for j in range(1, len(nums), 2):
-        #     odd_sum += nums[j]
-        # res = max(even_sum, odd_sum)
-        # return res
+    def rob(self, nums: List[int]) -> int:
+        self.memo = {}
+        return self.robFrom(0, nums)
+    
+    def robFrom(self, i, nums):
+        if i >= len(nums):
+            return 0
+        if i in self.memo:
+            return self.memo[i]
+        ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
+        self.memo[i] = ans
+        return ans
