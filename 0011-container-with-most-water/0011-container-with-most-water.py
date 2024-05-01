28630c2b237b1c6d19ba37dcd9959e098758d400
Sharanya <Sharanya.gandla05@gmail.com>
2024-04-30 23:35:36 -0700

    Time: 540 ms (30.41%), Space: 22.3 MB (5.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,12 +1,34 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+#         maxarea = 0
+#         for left in range(len(height)):
+#             for right in range(left + 1, len(height)):
+#                 width = right - left
+#                 maxarea = max(maxarea, min(height[left], height[right]) * width)
+
+#         return maxarea
+
         left, right = 0, len(height) - 1
-        area, max_area = 0, 0
-        while left <= right:
+        maxArea = 0
+        while left < right:
             area = (right - left) * min(height[left], height[right])
+            maxArea = max(area, maxArea)
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-            max_area = max(area, max_area)
-        return max_area
+        return maxArea
+        
+        
+#         1,8,6,2,5,4,8,3,7
+#           l             r
+        
+#         6*(1)=6
+        
+        
+        
+        
