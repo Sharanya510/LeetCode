e57b8802f4973e153864c9bbf90a51632b65d8a6
Sharanya <Sharanya.gandla05@gmail.com>
2024-05-01 21:48:07 -0700

    Time: 36 ms (49.70%), Space: 16.6 MB (41.77%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        seen = set()
+        for word in wordDict:
+            if s.startswith(word):
+                queue = collections.deque()
+                queue.append([len(word), "{} ".format(word)])
+                seen.add("{} ".format(word))
+                while queue:
+                    matched_length, sentence = queue.popleft()
+                    if matched_length == len(s):
+                        seen.add(sentence)
+                        res.append(sentence.strip(" "))
+                        continue
+                    rest_s = s[matched_length:]
+                    for word in wordDict:
+                        if rest_s.startswith(word):
+                            if sentence + "{} ".format(word) not in seen:
+                                queue.append([matched_length + len(word), sentence + "{} ".format(word)])
+                                seen.add(sentence + "{} ".format(word))
+        return res
