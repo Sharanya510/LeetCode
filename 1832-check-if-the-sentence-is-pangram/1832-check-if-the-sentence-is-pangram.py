fed5e92c85ba6cc93b86b40084e31a6153d2964c
Sharanya <Sharanya.gandla05@gmail.com>
2024-05-28 22:22:12 -0700

    Time: 30 ms (88.56%), Space: 16.5 MB (45.95%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,8 +1,19 @@
 from collections import Counter
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        seen = set(sentence)
-        return len(seen) == 26
+        
+        seen = [False] * 26
+        for curr_char in sentence:
+            seen[ord(curr_char) - ord('a')] = True
+        for status in seen:
+            if not status:
+                return False
+        return True
+        
+        
+        # METHOD - 2
+        # seen = set(sentence)
+        # return len(seen) == 26
     
         # METHOD -1
         # for i in range(26):
