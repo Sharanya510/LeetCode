7d90d158f95afcfc179e7cbd7b4192e1981464c6
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-08 12:41:02 -0700

    Time: 44 ms (61.20%), Space: 16.9 MB (86.30%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if not s:
-            return True
-        left , right = 0, len(s)-1
+        left = 0
+        right = len(s) - 1
         while left < right:
             while left < right and not s[left].isalnum():
                 left += 1
@@ -13,3 +12,4 @@ class Solution:
             left += 1
             right -= 1
         return True
+                
