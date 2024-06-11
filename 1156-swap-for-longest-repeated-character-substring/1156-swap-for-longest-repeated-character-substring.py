1b73380ca6cad728fdaaa1e75c27e3c751ccf55e
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-10 22:09:54 -0700

    Time: 48 ms (89.20%), Space: 18.7 MB (8.42%) - LeetHub

---
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxRepOpt1(self, text: str) -> int:
+        charMap = defaultdict(list)
+
+        l = 0
+        for r in range(len(text)):
+            if text[r] != text[l]:
+                charMap[text[l]].append([l, r - l])
+                l = r
+        charMap[text[-1]].append([l, r - l + 1])
+
+        # corner case: if there is only one element -> return length of the text
+        if len(charMap) == 1:
+            return len(text)
+
+        res = 0
+        for c, lst in charMap.items():
+            prevStart, prevCount = lst[0]
+            res = max(res, prevCount + (1 if len(lst) >= 2 else 0))
+            if len(lst) >= 2:
+                for curStart, curCount in lst[1:]:
+                    # if can connect
+                    one = len(lst) > 2
+                    if prevStart + prevCount + 1 == curStart: # 0 + 1 + 1 == 2
+                        res = max(res, prevCount + curCount + one)
+                    else:
+                        res = max(res, curCount + 1)
+
+                    prevStart, prevCount = curStart, curCount
+        return res 
