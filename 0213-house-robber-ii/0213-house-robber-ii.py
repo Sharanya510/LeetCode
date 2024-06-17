927667f9e12e215628f0ff174c8420b923a2fa74
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-17 13:32:36 -0700

    Time: 45 ms (5.37%), Space: 16.4 MB (93.69%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,13 +1,41 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0],self.helper_rob(nums[1:]), self.helper_rob(nums[:-1]) )
+        if len(nums) == 0 or nums is None:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(self.rob_simple(nums[:-1]), self.rob_simple(nums[1:]))
+
+    def rob_simple(self, nums: List[int]) -> int:
+        t1 = 0
+        t2 = 0
+        for current in nums:
+            temp = t1
+            t1 = max(current + t2, t1)
+            t2 = temp
+
+        return t1
     
-    def helper_rob(self, nums):
-        rob1, rob2 = 0, 0
-        #[rob1, rob2, n, n+1 , ....]
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
-            
+# 1   2   3   1
+# 0   1   2   3
+# max([1, 2, 3], [2, 3, 1])
+
+# 1, 2, 3
+# t1
+# t2
+# curr
+# temp = 1
+# t1 = 1+0 = 1
+# t2 = 1
+
+# curr = 2
+# temp = 1
+# t1 = max(2 + 1, 1) = 3
+# t2 = 1
+
+# curr = 3
+# temp = 3
+# t1 = max(3 + 1, 3) = 4
+# t2 = 3
