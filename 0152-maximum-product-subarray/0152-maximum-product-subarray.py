ddede241cd28cc00cfeca000e32070cd569491a6
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-17 14:53:12 -0700

    Time: 90 ms (5.12%), Space: 17 MB (74.87%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        ##keep track of max and min so far at every n, if n is 0 reset both to 1,1
-        max_so_far,min_so_far=1,1
-        result=max(nums)
-        for n in nums:
-            if n==0:
-                max_so_far,min_so_far=1,1
-                continue
-            temp=n*max_so_far
-            max_so_far=max(n,n*max_so_far,n*min_so_far)
-            min_so_far=min(n,temp,n*min_so_far)
-            result=max(result,max_so_far)
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+            max_so_far = temp_max
+            result = max(max_so_far, result)
         return result
-          
-        
-        
