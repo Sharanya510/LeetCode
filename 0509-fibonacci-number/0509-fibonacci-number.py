00485bbadfeb15633350318fe9abd81e9baa16f9
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-17 12:56:21 -0700

    Time: 41 ms (44.94%), Space: 16.5 MB (66.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,21 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return self.helper(n)
+        memory = [0] * (n+1)
+        if n < 2: 
+            return n
+        memory[1] = 1
+        for num in range(2, n+1):
+            memory[num] = memory[num-1] + memory[num-2]
+        return memory[num]
+
+        
+        # APPROACH 1
+#         return self.helper(n)
     
-    def helper(self, n):
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        return self.helper(n-1) + self.helper(n-2)
+#     def helper(self, n):
+#         if n == 0:
+#             return 0
+#         if n == 1:
+#             return 1
+#         return self.helper(n-1) + self.helper(n-2)
         
