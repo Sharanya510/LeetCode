99f82a9fde2a9be6585aec37641a3f6de57bdb97
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-16 20:50:07 -0700

    Time: 46 ms (6.39%), Space: 16.5 MB (47.70%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,19 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        
         for i in range(32):
-            # print(res)
             if n % 2 == 1:
                 res <<= 1
-                # print("res value before: " ,res)
                 res += 1
-                # print("res value before: " ,res)
             else:
                 res <<= 1
-            # print("n value before: " ,n)
             n >>= 1
-            # print("n value after: " ,n)
         return res
     
     
