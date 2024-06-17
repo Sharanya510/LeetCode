796c16e3f86e0e4d57f62f501f85eb87add4661f
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-17 13:10:18 -0700

    Time: 35 ms (51.52%), Space: 16.4 MB (67.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,46 @@
 class Solution:
-    memo = {1:1, 2:2}
     def climbStairs(self, n: int) -> int:
-        if n in self.memo:
-            return self.memo[n]
+        memo = [0] * (n + 1)
+        return self.climb_Stairs(0, n, memo)
+
+    def climb_Stairs(self, i: int, n: int, memo: List[int]) -> int:
+        if i > n:
+            return 0
+        if i == n:
+            return 1
+        if memo[i] > 0:
+            return memo[i]
+        memo[i] = self.climb_Stairs(i + 1, n, memo) + self.climb_Stairs(i + 2, n, memo)
+        return memo[i]
+
+
+# APPROACH -- BOTTOM UP -- ITERATIVE
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n == 1: return 1
+#         if n == 2: return 2
+#         dp = [0]  * (n + 1)
+#         dp[1] = 1
+#         dp[2] = 2
+#         for i in range(3, n+1):
+#             dp[i] = dp[i - 1] + dp[i - 2]
+#         return dp[n]
         
-        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
         
-        return self.memo[n]
+        
+        
+#         return self.helper(n)
+    
+#     def helper(self, n):
+#         if n == 1:
+#             return 1
+#         if n == 2:
+#             return 2
+#         if n <= 0:
+#             return 0
+#         return self.helper(n-1)+ self.helper(n-2)
+        
+        
+        
+        
         
