a869c8c238954abca9302ffaa3958cd6496f7daf
Sharanya <Sharanya.gandla05@gmail.com>
2024-06-17 15:12:45 -0700

    Time: 34 ms (88.12%), Space: 16.6 MB (51.99%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,31 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        words = set(wordDict)
-        queue = deque([0])
-        seen = set()
-
-        while queue:
-            start = queue.popleft()
-            if start == len(s):
-                return True
-
-            for end in range(start + 1, len(s) + 1):
-                if end in seen:
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordDict:
+                # Handle out of bounds case
+                if i < len(word) - 1:
                     continue
 
-                if s[start:end] in words:
-                    queue.append(end)
-                    seen.add(end)
+                if i == len(word) - 1 or dp[i - len(word)] == True:
+                    if s[i - len(word) + 1 : i + 1] == word:
+                        dp[i] = True
+                        break
 
-        return False
+        return dp[-1]
+        
+        
+        # words = set(wordDict)
+        # queue = deque([0])
+        # seen = set()
+        # while queue:
+        #     start = queue.popleft()
+        #     if start == len(s):
+        #         return True
+        #     for end in range(start + 1, len(s) + 1):
+        #         if end in seen:
+        #             continue
+        #         if s[start:end] in words:
+        #             queue.append(end)
+        #             seen.add(end)
+        # return False
