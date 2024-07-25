6f62c8871491a09ff1d94313ad7cefe23d676fd0
Sharanya <Sharanya.gandla05@gmail.com>
2024-07-25 16:17:38 -0700

    Time: 587 ms (87.04%), Space: 29.5 MB (17.70%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,33 +1,22 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        # Function to heapify a subtree (in top-down order) rooted at index i.
-        def heapify(n: int, i: int):
-            # Initialize largest as root 'i'.
-            largest = i;
-            left = 2 * i + 1
-            right = 2 * i + 2
-            # If left child is larger than root.
-            if left < n and nums[left] > nums[largest]:
-                largest = left
-            # If right child is larger than largest so far.
-            if right < n and nums[right] > nums[largest]:
-                largest = right
-            # If largest is not root swap root with largest element
-            # Recursively heapify the affected sub-tree (i.e. move down).
-            if largest != i:
-                nums[i], nums[largest] =  nums[largest], nums[i]
-                heapify(n, largest)
+        def counting_sort():
+            # Create the counting hash map.
+            counts = {}
+            # Find the minimum and maximum values in the array.
+            minVal, maxVal = min(nums), max(nums)
+            # Update element's count in the hash map.
+            for val in nums:
+                counts[val] = counts.get(val, 0) + 1
 
-        def heap_sort():
-            n = len(nums)
-            # Build heap; heapify (top-down) all elements except leaf nodes.
-            for i in range(n // 2 - 1, -1, -1):
-                heapify(n, i)
-            # Traverse elements one by one, to move current root to end, and
-            for i in range(n - 1, -1, -1):
-                nums[0], nums[i] = nums[i], nums[0]
-                # call max heapify on the reduced heap.
-                heapify(i, 0)
+            index = 0
+            # Place each element in its correct position in the array.
+            for val in range(minVal, maxVal + 1, 1):
+                # Append all 'val's together if they exist.
+                while counts.get(val, 0) > 0:
+                    nums[index] = val
+                    index += 1
+                    counts[val] -= 1
 
-        heap_sort()
+        counting_sort()
         return nums
