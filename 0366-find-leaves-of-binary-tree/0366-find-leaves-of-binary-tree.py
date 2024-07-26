b14743673b27c11146d8b538b670823bf0c15788
Sharanya <Sharanya.gandla05@gmail.com>
2024-07-25 23:23:14 -0700

    Time: 37 ms (44.74%), Space: 16.5 MB (59.11%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
@@ -9,11 +9,11 @@
 # from typing import List, Tuple
 # from collections import defaultdict
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
     def __init__(self):
