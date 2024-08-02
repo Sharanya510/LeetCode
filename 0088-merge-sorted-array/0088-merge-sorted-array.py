bc0987dff8f5c40b0793d4caf13fd98f139ad345
Sharanya <Sharanya.gandla05@gmail.com>
2024-08-01 18:45:51 -0700

    Time: 28 ms (98.39%), Space: 16.5 MB (44.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,47 +1,19 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        # for i in range(n):
-        #     nums1[i+m] = nums2[i]
-        # nums1.sort()
+        p3=m+n-1
         
-        p1 = m -1
-        p2 = n -1
-        i = m+n-1
-        while p1 >=0 and p2 >=0:
-                if nums1[p1] < nums2[p2]:
-                    print(nums1)
-                    nums1[i] = nums2[p2]
-                    p2 -= 1
-                    i -= 1
-                elif nums1[p1] == nums2[p2]:
-                    print(nums1)
-                    nums1[i] = nums1[p1]
-                    nums1[i-1] = nums2[p2]
-                    p1 -=1
-                    p2 -=1
-                    i -= 2
-                else:
-                    print(nums1)
-                    nums1[i] = nums1[p1]
-                    p1 -=1
-                    i -= 1
-      
-        while p2 >=0:
-            nums1[i] = nums2[p2]
-            p2 -= 1
-            i-=1
-        while p1 >=0 :
-            nums1[i] = nums1[p1]
-            p1 -= 1
-            i-=1
-        return nums1
-
-    
-    
-         
+        while m >0 and n>0:
         
-
+            if nums1[m-1] > nums2[n-1]:
+                nums1[p3] = nums1[m-1]
+                m-=1
+            else:
+                nums1[p3] = nums2[n-1]
+                n-=1
+            p3-=1
+        
+        while n>0:
+            nums1[p3] = nums2[n-1]
+            p3-=1
+            n-=1
             
